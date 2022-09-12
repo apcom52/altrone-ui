abb30741a9f1fbaeb362b9557411b33dc9c6c4d8
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-09-12 18:28:38 +0300

    1000. Set default constants

---
+++ src/constants/Offset.ts
@@ -0,0 +1,19 @@
+import {Margin, Padding} from "../hooks";
+import {Offset} from "../hooks/useOffset/useOffset";
+
+export const DEFAULT_OFFSET = 4
+export const ZERO_MARGIN: Offset = {}
+export const ZERO_PADDING: Offset = {}
+
+export const VERTICAL_MARGIN: Offset = {
+  vertical: 1
+}
+
+export const HORIZONTAL_MARGIN: Offset = {
+  horizontal: 1
+}
+
+export const PADDING: Offset = {
+  vertical: 1,
+  horizontal: 1
+}
