b00c5eb79dee39f6290cac8a54f0696352f67d1f
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-09-16 23:16:20 +0300

    1002. styles and tests for paragraph

--- src/components/typography/Paragraph/Paragraph.tsx
+++ src/components/typography/Paragraph/Paragraph.tsx
@@ -1,16 +1,19 @@
 import {WithAltroneOffsets, WithoutDefaultOffsets} from "../../../types";
 import {memo} from "react";
 import {Box} from "../../containers/Box";
+import './paragraph.scss'
+import clsx from "clsx";
 
 interface ParagraphProps extends WithoutDefaultOffsets, WithAltroneOffsets {}
 
-const Blockquote = ({ children, ...props }: ParagraphProps) => {
+const Paragraph = ({ children, className, ...props }: ParagraphProps) => {
   return <Box
     tagName='p'
+    className={clsx('alt-paragraph', className)}
     {...props}
   >
     {children}
   </Box>
 }
 
-export default memo(Blockquote)
+export default memo(Paragraph)
