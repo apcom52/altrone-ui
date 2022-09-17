1f9f9e489ac32d4d19d9e7ce09d4a5d6d5daaa6c
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-09-17 16:31:37 +0300

    1003. implemented button styles

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import {withAltrone} from "../../hocs";
 import {Theme} from "../../types";
 import {Button} from "./Button";
+import {ButtonStyle} from "./Button/Button";
 
 const Template = ({component, dark, ...args}) => {
   return withAltrone(component, {
@@ -12,10 +13,27 @@ export const ButtonExample = Template.bind({})
 ButtonExample.args = {
   component: Button,
   children: 'Action button',
+  disabled: false,
+  style: ButtonStyle.default,
   dark: false
+}
+
+ButtonExample.argTypes = {
+  style: {
+    control: 'select',
+    options: [ButtonStyle.default, ButtonStyle.primary, ButtonStyle.success, ButtonStyle.danger]
+  },
 }
 
 export default {
   component: ButtonExample,
-  title: 'Button'
+  title: 'Button',
+  parameters: {
+    style: [
+      { name: 'default', value: ButtonStyle.default },
+      { name: 'primary', value: ButtonStyle.primary },
+      { name: 'success', value: ButtonStyle.success },
+      { name: 'danger', value: ButtonStyle.danger },
+    ]
+  }
 }
--- src/components/button/Button/Button.tsx
+++ src/components/button/Button/Button.tsx
@@ -1,11 +1,47 @@
 import {memo} from "react";
+import {WithAltroneOffsets, WithoutDefaultOffsets} from "../../../types";
+import clsx from "clsx";
+import {Box} from "../../containers";
+import './button.scss'
 
-interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
-
+export enum ButtonStyle {
+  default,
+  primary = 'primary',
+  success = 'success',
+  danger = 'danger',
 }
 
-const Button = ({ children }: ButtonProps) => {
-  return <button>{children}</button>
+export enum ButtonVariant {
+  default,
+  borders = 'borders',
+  transparent = 'transparent',
+  text = 'text'
+}
+
+interface ButtonProps extends Omit<WithoutDefaultOffsets<React.HTMLProps<HTMLButtonElement>>, 'style' | 'target'>, WithAltroneOffsets {
+  style?: ButtonStyle
+  variant?: ButtonVariant
+  href?: string
+  target?: HTMLAnchorElement['target'],
+}
+
+const ButtonComponents = [
+  'button',
+  'a'
+]
+
+const Button = ({ children, style = ButtonStyle.default, variant = ButtonVariant.default, href, className, ...props }: ButtonProps) => {
+  return <Box
+    tagName={ButtonComponents[href ? 1 : 0] as keyof JSX.IntrinsicElements}
+    className={clsx('alt-button', className, {
+      [`alt-button--style-${style}`]: style !== ButtonStyle.default,
+      [`alt-button--variant-${variant}`]: variant !== ButtonVariant.default,
+    })}
+    href={href}
+    {...props}
+  >
+    {children}
+  </Box>
 }
 
 export default memo(Button)
