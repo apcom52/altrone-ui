c41eacb7c225daf569da40337574963ca9065557
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-09-20 09:49:04 +0300

    Build 2238. styles for TextInput islands

--- src/components/form/TextInput/TextInput.tsx
+++ src/components/form/TextInput/TextInput.tsx
@@ -1,4 +1,4 @@
-import {memo, useMemo} from "react";
+import {memo, useEffect, useMemo, useRef, useState} from "react";
 import {WithAltroneOffsets, WithoutDefaultOffsets} from "../../../types";
 import './text-input.scss';
 import clsx from "clsx";
@@ -35,20 +35,48 @@ interface TextInputProps extends WithoutDefaultOffsets<React.HTMLProps<HTMLInput
   rightIcon?: JSX.Element
 }
 
-const TextInput = ({ className, classNames = {}, prefix, leftIcon, leftIsland, suffix, rightIcon, rightIsland, ...props}: TextInputProps) => {
+const TextInput = ({ className, classNames = {}, prefix, leftIcon, leftIsland, suffix, rightIcon, rightIsland, style, ...props}: TextInputProps) => {
   const _leftIsland = useInputIsland(leftIsland, leftIcon, prefix)
   const _rightIsland = useInputIsland(rightIsland, rightIcon, suffix)
 
-  console.log('left', _leftIsland);
-  console.log('right', _rightIsland);
+  const leftIslandRef = useRef<HTMLDivElement>(null)
+  const rightIslandRef = useRef<HTMLDivElement>(null)
 
-  return <div className={clsx('alt-text-input', className)}>
+  const [leftPadding, setLeftPadding] = useState(12)
+  const [rightPadding, setRightPadding] = useState(12)
+
+  useEffect(() => {
+    if (leftIslandRef.current) {
+      const leftIslandRect = leftIslandRef.current.getBoundingClientRect()
+      setLeftPadding(8 + leftIslandRect.width + 8)
+    } else {
+      setLeftPadding(12)
+    }
+  }, [_leftIsland, leftIslandRef.current])
+
+  useEffect(() => {
+    if (rightIslandRef.current) {
+      const rightIslandRect = rightIslandRef.current.getBoundingClientRect()
+      setRightPadding(8 + rightIslandRect.width + 8)
+    } else {
+      setRightPadding(12)
+    }
+  }, [_rightIsland, rightIslandRef.current])
+
+  return <div
+    className={clsx('alt-text-input', className)}
+  >
     <input
       className={clsx('alt-text-input__control', classNames.control)}
       {...props}
+      style={{
+        ...style,
+        paddingLeft: leftPadding,
+        paddingRight: rightPadding
+      }}
     />
-    { _leftIsland && <div className='alt-text-input__left-island'>{_leftIsland}</div> }
-    { _rightIsland && <div className='alt-text-input__right-island'>{_rightIsland}</div> }
+    { _leftIsland && <div className='alt-text-input__left-island' ref={leftIslandRef}>{_leftIsland}</div> }
+    { _rightIsland && <div className='alt-text-input__right-island' ref={rightIslandRef}>{_rightIsland}</div> }
   </div>
 }
 
