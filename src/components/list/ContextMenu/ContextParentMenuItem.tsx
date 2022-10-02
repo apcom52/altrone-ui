931fe3c4f2785aee7cba766c7235c36900924cb0
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-10-02 23:38:27 +0300

    Build 2239: Tests

--- src/components/list/ContextMenu/ContextParentMenuItem.tsx
+++ src/components/list/ContextMenu/ContextParentMenuItem.tsx
@@ -7,16 +7,16 @@ import {ContextMenu} from "./index";
 
 const ContextParentMenuItem = (action: ParentContextAction) => {
   const [isChildrenContextMenuVisible, setIsChildrenContextMenuVisible] = useState(false)
-  const itemRef = useRef<HTMLDivElement>(null)
+  const itemRef = useRef<HTMLButtonElement>(null)
 
   const altroneRef = useRef(document.body.querySelector('.altrone'))
 
   return <>
-    <div className='alt-context-menu-item' ref={itemRef} onClick={() => setIsChildrenContextMenuVisible(true)}>
+    <button className='alt-context-menu-item' ref={itemRef} onClick={() => setIsChildrenContextMenuVisible(true)}>
       <div className='alt-context-menu-item__icon'>{action.icon}</div>
       <div className='alt-context-menu-item__title'>{action.title}</div>
       <div className='alt-context-menu-item__childrenArrow'><Icon i='keyboard_arrow_right' /></div>
-    </div>
+    </button>
     {isChildrenContextMenuVisible && createPortal(<FloatingBox targetRef={itemRef.current} placement='right' onClose={() => setIsChildrenContextMenuVisible(false)} offset={8}>
       <ContextMenu menu={action.children} />
     </FloatingBox>, altroneRef.current)}
