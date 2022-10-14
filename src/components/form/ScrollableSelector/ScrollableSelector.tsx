3b0d35f295d0efbe8fc12d9099f4328808ca015a
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-10-14 23:38:23 +0300

    2241: added ScrollableSelector

---
+++ src/components/form/ScrollableSelector/ScrollableSelector.tsx
@@ -0,0 +1,47 @@
+import {memo, useEffect, useRef} from "react";
+import {Option} from "../../../types";
+import {Align} from "../../../types/Align";
+import clsx from "clsx";
+import './scrollable-selector.scss';
+
+interface ScrollableSelectorProps {
+  value: string | number
+  options: Option[]
+  onChange: (value: string | number) => void
+  disabled?: boolean
+  width?: number | string
+  align?: Align
+}
+
+const ScrollableSelector = ({ value, options = [], width = '100%', disabled = false, align = Align.center, onChange }: ScrollableSelectorProps) => {
+  const selectorRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const valueIndex = options.findIndex(option => option.value === value)
+
+    selectorRef.current?.scrollTo({
+      top: 34 * valueIndex,
+      behavior: 'smooth'
+    })
+  }, [value])
+
+  return <div ref={selectorRef} className={clsx('alt-scrollable-selector', {
+    'alt-scrollable-selector--align-start': align === Align.start,
+    'alt-scrollable-selector--align-end': align === Align.end,
+  })} style={{ width }}>
+    {options.map((option, optionIndex) => (
+      <button
+        key={optionIndex}
+        className={clsx('alt-scrollable-selector__option', {
+          'alt-scrollable-selector__option--selected': value === option.value
+        })}
+        onClick={() => onChange(option.value)}
+      >
+        {option.label}
+      </button>
+    ))}
+    <div className='alt-scrollable-selector__footer' />
+  </div>
+}
+
+export default memo(ScrollableSelector)
