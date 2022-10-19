d7c10c846aa759a78b2ebedd34c9c20b463e6873
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-10-20 00:10:50 +0300

    2242: Segmented Progress

--- src/components/indicators/Progress/Progress.tsx
+++ src/components/indicators/Progress/Progress.tsx
@@ -21,11 +21,17 @@ const Progress = ({ variant = ProgressVariant.default, value = 0, max = 100, dan
   return <div className={clsx('alt-progress', {
     'alt-progress--size-small': size === Size.small,
     'alt-progress--size-large': size === Size.large,
+    'alt-progress--variant-segmented': variant === ProgressVariant.segmented
   })}>
-    <div
+    {variant === ProgressVariant.default && <div
       className='alt-progress__active'
       style={{ width: percent + '%' }}
-    />
+    />}
+    {variant === ProgressVariant.segmented && (new Array(max).fill(0).map((_, segmentIndex) => {
+      return <div className={clsx('alt-progress__segment', {
+        'alt-progress__segment--active': segmentIndex < value
+      })} key={segmentIndex}></div>
+    }))}
   </div>
 }
 
