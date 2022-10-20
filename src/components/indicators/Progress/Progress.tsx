b4f93350d5a5507ca7ae493fd7f403cc6a98fec7
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-10-20 23:17:05 +0300

    2242: Progress tests and custom className for Progress

--- src/components/indicators/Progress/Progress.tsx
+++ src/components/indicators/Progress/Progress.tsx
@@ -15,27 +15,43 @@ interface ProgressProps {
   size?: Size
   role?: Role
   variant?: ProgressVariant
+  className?: string
 }
 
-const Progress = ({ variant = ProgressVariant.default, value = 0, max = 100, role = Role.default, size = Size.medium }) => {
+const Progress = ({
+  variant = ProgressVariant.default,
+  value = 0,
+  max = 100,
+  role = Role.default,
+  size = Size.medium,
+  className
+}: ProgressProps) => {
   const percent = Math.round(value / max * 100)
 
-  return <div className={clsx('alt-progress', {
-    'alt-progress--size-small': size === Size.small,
-    'alt-progress--size-large': size === Size.large,
-    'alt-progress--variant-segmented': variant === ProgressVariant.segmented,
-    'alt-progress--primary': role === Role.primary,
-    'alt-progress--success': role === Role.success,
-    'alt-progress--danger': role === Role.danger
-  })}>
+  return <div
+    className={clsx('alt-progress', className, {
+      'alt-progress--size-small': size === Size.small,
+      'alt-progress--size-large': size === Size.large,
+      'alt-progress--variant-segmented': variant === ProgressVariant.segmented,
+      'alt-progress--primary': role === Role.primary,
+      'alt-progress--success': role === Role.success,
+      'alt-progress--danger': role === Role.danger
+    })}
+    data-testid='alt-test-progress'
+  >
     {variant === ProgressVariant.default && <div
       className='alt-progress__active'
       style={{ width: percent + '%' }}
+      data-testid='alt-test-progress-active'
     />}
     {variant === ProgressVariant.segmented && (new Array(max).fill(0).map((_, segmentIndex) => {
-      return <div className={clsx('alt-progress__segment', {
-        'alt-progress__segment--active': segmentIndex < value
-      })} key={segmentIndex}></div>
+      return <div
+        key={segmentIndex}
+        className={clsx('alt-progress__segment', {
+          'alt-progress__segment--active': segmentIndex < value
+        })}
+        data-testid='alt-test-progress-segment'
+      />
     }))}
   </div>
 }
