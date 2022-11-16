159372d0c3d498176e4fe11b23703c628109ede5
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-11-16 19:32:09 +0300

    2246: adaptive header, sorting and filtering for DataTable

--- src/components/data/DataTable/DataTableHeader.tsx
+++ src/components/data/DataTable/DataTableHeader.tsx
@@ -9,11 +9,15 @@ import {Icon} from "../../icons";
 import DataTableFiltering from "./DataTableFiltering";
 import {useDataTableContext} from "../../../contexts";
 import {useWindowSize} from "../../../hooks";
+import {FloatingBoxMobileBehaviour} from "../../containers/FloatingBox/FloatingBox";
+import {Role} from "../../../types";
 
 const DataTableHeader = () => {
   const { search, setSearch, sortKeys, sortBy, columns, appliedFilters, filters, searchBy } = useDataTableContext()
   const [isSortVisible, setIsSortVisible] = useState(false)
   const [isFilterVisible, setIsFilterVisible] = useState(false)
+  const [isSearchVisible, setIsSearchVisible] = useState(false)
+
   const { ltePhoneL } = useWindowSize()
 
   const currentSortingColumn = useMemo(() => {
@@ -34,47 +38,73 @@ const DataTableHeader = () => {
   }
 
   return <div className='alt-data-table-header'>
-    <div className='alt-data-table-header__filters'>
-      {sortKeys.length > 0 && (
-        <Button
-          ref={sortRef}
-          leftIcon={ltePhoneL ? null : <Icon i='swap_vert' />}
+    {ltePhoneL && !isSearchVisible && <div className='alt-data-table-header__filters'>
+        {sortKeys.length > 0 && (
+          <Button
+            ref={sortRef}
+            leftIcon={ltePhoneL ? null : <Icon i='swap_vert' />}
+            variant={ButtonVariant.transparent}
+            onClick={() => setIsSortVisible(true)}
+            isIcon={ltePhoneL}
+          >
+            {ltePhoneL
+              ? <Icon i='swap_vert' />
+              : <>{sortBy ? 'Sorted' : 'Sort'} by {currentSortingColumn && <strong className='alt-data-table-header__filter-value'>{currentSortingColumn.label || currentSortingColumn.accessor}</strong>}</>
+            }
+          </Button>
+        )}
+        {filters.length > 0 && <Button
+          ref={filterRef}
+          leftIcon={ltePhoneL ? null : <Icon i='tune' style='outlined' />}
           variant={ButtonVariant.transparent}
-          onClick={() => setIsSortVisible(true)}
+          onClick={() => setIsFilterVisible(true)}
           isIcon={ltePhoneL}
+          role={ltePhoneL && appliedFilters.length > 0 && Role.primary}
         >
           {ltePhoneL
-            ? <Icon i='swap_vert' />
-            : <>{sortBy ? 'Sorted' : 'Sort'} by {currentSortingColumn && <strong className='alt-data-table-header__filter-value'>{currentSortingColumn.label || currentSortingColumn.accessor}</strong>}</>
+            ? <Icon i='tune' style='outlined' />
+            : <>Filters {appliedFilters.length > 0 && <strong className='alt-data-table-header__filter-value'>({appliedFilters.length})</strong>}</>
           }
-        </Button>
-      )}
-      {filters.length > 0 && <Button
-        ref={filterRef}
-        leftIcon={ltePhoneL ? null : <Icon i='tune' style='outlined' />}
-        variant={ButtonVariant.transparent}
-        onClick={() => setIsFilterVisible(true)}
-        isIcon={ltePhoneL}
-      >
-        {ltePhoneL
-          ? <Icon i='tune' style='outlined' />
-          : <>Filters {appliedFilters.length > 0 && <strong className='alt-data-table-header__filter-value'>({appliedFilters.length})</strong>}</>
-        }
-      </Button>}
-    </div>
-    {searchBy
+        </Button>}
+    </div>}
+    {(searchBy && !isSearchVisible)
       ?
         ltePhoneL
-          ? <Button variant={ButtonVariant.transparent} isIcon><Icon i='search' /></Button>
+          ? <Button variant={ButtonVariant.transparent} isIcon onClick={() => setIsSearchVisible(true)}><Icon i='search' /></Button>
           : <div className='alt-data-table-header__search' data-testid='alt-test-datatable-search'>
             <TextInput placeholder='Search' value={search} onChange={setSearch} />
           </div>
       : null
     }
-    {isSortVisible && sortKeys.length && <FloatingBox targetRef={sortRef.current} onClose={closeSortingPopup} minWidth={250} useParentWidth>
+    {(searchBy && isSearchVisible)
+      ? <>
+          <Button
+            ref={sortRef}
+            variant={ButtonVariant.transparent}
+            onClick={() => setIsSearchVisible(false)}
+            isIcon={ltePhoneL}
+          >
+            <Icon i='arrow_forward_ios' />
+          </Button>
+
+          <div className='alt-data-table-header__search' data-testid='alt-test-datatable-search'>
+            <TextInput placeholder='Search' value={search} onChange={setSearch} />
+          </div>
+          <Button
+            ref={sortRef}
+            variant={ButtonVariant.transparent}
+            onClick={() => setSearch('')}
+            isIcon={ltePhoneL}
+          >
+            <Icon i='backspace' />
+          </Button>
+        </>
+      : null
+    }
+    {isSortVisible && sortKeys.length && <FloatingBox targetRef={sortRef.current} onClose={closeSortingPopup} minWidth={250} mobileBehaviour={FloatingBoxMobileBehaviour.modal} useParentWidth>
       <DataTableSorting onClose={closeSortingPopup} />
     </FloatingBox>}
-    {isFilterVisible && <FloatingBox targetRef={filterRef.current} onClose={closeFilteringPopup} minWidth={250} useParentWidth>
+    {isFilterVisible && <FloatingBox targetRef={filterRef.current} onClose={closeFilteringPopup} minWidth={250} mobileBehaviour={FloatingBoxMobileBehaviour.modal} useParentWidth>
       <DataTableFiltering onClose={closeFilteringPopup} />
     </FloatingBox>}
   </div>
