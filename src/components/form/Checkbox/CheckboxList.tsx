ad0d1d1d8b804ff2c7f13c135333161a5c704f3c
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-11-23 19:26:07 +0300

    2247: Show more/show less for CheckboxList

--- src/components/form/Checkbox/CheckboxList.tsx
+++ src/components/form/Checkbox/CheckboxList.tsx
@@ -1,17 +1,38 @@
-import {memo} from "react";
+import {memo, useState} from "react";
 import {Direction} from "../../../types";
 import clsx from "clsx";
 import './checkbox-list.scss';
 
 interface CheckboxListProps extends React.HTMLProps<HTMLDivElement> {
   direction?: Direction
+  limit?: number
 }
 
-const CheckboxList = ({ children, direction = Direction.horizontal, className, ...props }: CheckboxListProps) => {
+const CheckboxList = ({ children = [], direction = Direction.horizontal, className, limit = 5, ...props }: CheckboxListProps) => {
+  const length = Array.isArray(children) ? children.length : 1
+  const [isExpanded, setIsExpanded] = useState(length <= limit)
+
+  const onExpandClick = () => {
+    setIsExpanded(state => !state)
+  }
+
+  let validLimit = limit > 1 ? limit : 1
+
+  const visibleChildren =  Array.isArray(children)
+    ? isExpanded
+      ? children
+      : children.slice(0, validLimit)
+    : children
+
   return <div className={clsx('alt-checkbox-list', className, {
     'alt-checkbox-list--vertical': direction === Direction.vertical
   })} {...props}>
-    {children}
+    {visibleChildren}
+    {length > limit && <div className='alt-checkbox-list__actions'>
+      <button className='alt-checkbox-list__expand' onClick={onExpandClick}>
+        {isExpanded ? 'Show less' : 'Show more'}
+      </button>
+    </div>}
   </div>
 }
 
