6d24d55a87ecdf560a0d163b9c3f9f0a0b4b01ec
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-11-25 13:53:16 +0300

    2247: localization for DataTable

--- src/components/data/DataTable/DataTableFooter.tsx
+++ src/components/data/DataTable/DataTableFooter.tsx
@@ -2,17 +2,18 @@ import {memo} from "react";
 import './data-table-footer.scss';
 import DataTablePagination from "./DataTablePagination";
 import {useDataTableContext} from "../../../contexts";
-import {useWindowSize} from "../../../hooks";
+import {useLocalization, useWindowSize} from "../../../hooks";
 
 const DataTableFooter = () => {
   const { data, initialData, limit } = useDataTableContext()
   const { ltePhoneL } = useWindowSize()
+  const t = useLocalization()
 
   return <div className='alt-data-table-footer'>
     <div className='alt-data-table-footer__status' data-testid='alt-test-datatable-status'>
       {ltePhoneL
-        ? `${data.length} ${data.length === 1 ? 'line' : 'lines'}`
-        : <>Showing {data.length} {data.length === 1 ? 'line' : 'lines'} {data.length !== initialData.length && `(Total: ${initialData.length})`}</>
+        ? t('data.dataTable.lines', { plural: true, value: data.length, vars: { count: data.length } })
+        : <>{t('data.dataTable.showing')} {t('data.dataTable.lines', { plural: true, value: data.length, vars: { count: data.length } })}</>
       }
     </div>
     {initialData.length > limit && <div className='alt-data-table-footer__pagination'>
