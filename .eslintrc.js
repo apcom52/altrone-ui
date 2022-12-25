199004bc561b24788821559e48ac9c148e5ca2bc
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-12-25 13:35:05 +0300

    build(2251): added config for eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:prettier/recommended",
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab'],
+		"linebreak-style": ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
+};
