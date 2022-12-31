3d7940d8d8209bffbc0cb2efc7cb2dab431fdafd
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-12-31 16:55:09 +0300

    docs(2252): updated readme

--- dist/types/components/form/Checkbox/Checkbox.d.ts
+++ dist/types/components/form/Checkbox/Checkbox.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="react" />
-import './checkbox.scss';
+import "./checkbox.scss";
 import { BasicInputProps } from "../BasicInput";
-interface CheckboxProps extends Omit<React.HTMLProps<HTMLInputElement>, 'onChange' | 'size'>, BasicInputProps {
+interface CheckboxProps extends Omit<React.HTMLProps<HTMLInputElement>, "onChange" | "size" | "ref">, Omit<BasicInputProps, "size"> {
     danger?: boolean;
     CheckIconComponent?: JSX.Element;
     onChange: (checked: boolean) => void;
