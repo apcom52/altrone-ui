dc889048cd0329965b0129a83afa8243f6350cd9
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2023-01-04 17:24:57 +0300

    fix(2301): fixed issue with storybook and DOM tags for DataTableFooter

--- src/components/data/DataTable/DataTableFooter.tsx
+++ src/components/data/DataTable/DataTableFooter.tsx
@@ -1,29 +1,54 @@
-import {memo} from "react";
-import './data-table-footer.scss';
+import { memo } from "react";
+import "./data-table-footer.scss";
 import DataTablePagination from "./DataTablePagination";
-import {useDataTableContext} from "../../../contexts";
-import {useLocalization, useWindowSize} from "../../../hooks";
+import { useDataTableContext } from "../../../contexts";
+import { useLocalization, useWindowSize } from "../../../hooks";
 
 const DataTableFooter = () => {
-  const { data, initialData, limit, columns, mobileColumns } = useDataTableContext()
-  const { ltePhoneL } = useWindowSize()
-  const t = useLocalization()
+  const { data, initialData, limit, columns, mobileColumns } =
+    useDataTableContext();
+  const { ltePhoneL } = useWindowSize();
+  const t = useLocalization();
 
-  return <tfoot>
-    <td className='alt-data-table-footer-wrapper' colSpan={ltePhoneL ? mobileColumns.length + 1 : columns.length}>
-      <div className='alt-data-table-footer'>
-        <div className='alt-data-table-footer__status' data-testid='alt-test-datatable-status'>
-          {ltePhoneL
-            ? t('data.dataTable.lines', { plural: true, value: data.length, vars: { count: data.length } })
-            : <>{t('data.dataTable.showing')} {t('data.dataTable.lines', { plural: true, value: data.length, vars: { count: data.length } })}</>
-          }
-        </div>
-        {initialData.length > limit && <div className='alt-data-table-footer__pagination'>
-          <DataTablePagination />
-        </div>}
-      </div>
-    </td>
-  </tfoot>
-}
+  return (
+    <tfoot>
+      <tr>
+        <td
+          className="alt-data-table-footer-wrapper"
+          colSpan={ltePhoneL ? mobileColumns.length + 1 : columns.length}
+        >
+          <div className="alt-data-table-footer">
+            <div
+              className="alt-data-table-footer__status"
+              data-testid="alt-test-datatable-status"
+            >
+              {ltePhoneL ? (
+                t("data.dataTable.lines", {
+                  plural: true,
+                  value: data.length,
+                  vars: { count: data.length },
+                })
+              ) : (
+                <>
+                  {t("data.dataTable.showing")}{" "}
+                  {t("data.dataTable.lines", {
+                    plural: true,
+                    value: data.length,
+                    vars: { count: data.length },
+                  })}
+                </>
+              )}
+            </div>
+            {initialData.length > limit && (
+              <div className="alt-data-table-footer__pagination">
+                <DataTablePagination />
+              </div>
+            )}
+          </div>
+        </td>
+      </tr>
+    </tfoot>
+  );
+};
 
-export default memo(DataTableFooter)
+export default memo(DataTableFooter);
