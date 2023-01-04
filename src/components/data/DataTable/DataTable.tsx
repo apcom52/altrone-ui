3e134a55740dfa6fbaafd21b5919494d14125c5c
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2023-01-04 19:34:34 +0300

    fix(2301): fixed wrong type of DataTableCell.Component prop

--- src/components/data/DataTable/DataTable.tsx
+++ src/components/data/DataTable/DataTable.tsx
@@ -1,121 +1,162 @@
-import {memo, ReactNode, useMemo, useState} from "react";
+import { memo, useMemo, useState } from "react";
 import DataTableHeader from "./DataTableHeader";
-import './data-table.scss';
+import "./data-table.scss";
 import DataTableBody from "./DataTableBody";
 import DataTableHeaderRow from "./DataTableHeaderRow";
 import DataTableFooter from "./DataTableFooter";
-import {DataTableAppliedFilter, DataTableContext, DataTableFilter} from "../../../contexts";
-import {Sort} from "../../../types";
+import {
+  DataTableAppliedFilter,
+  DataTableContext,
+  DataTableFilter,
+} from "../../../contexts";
+import { Sort } from "../../../types";
 import {
   DataTableSearchFunc,
   DataTableSortFunc,
   defaultCheckboxesFilter,
   defaultSearchFunc,
   defaultSelectFilter,
-  defaultSortFunc
+  defaultSortFunc,
 } from "./functions";
 import clsx from "clsx";
+import { DataTableCellProps } from "./DataTableCell";
 
 export interface DataTableColumn {
-  accessor: string
-  label?: string
-  width?: number | string
-  Component?: ReactNode
+  accessor: string;
+  label?: string;
+  width?: number | string;
+  Component?: React.FC<DataTableCellProps>;
 }
 
 interface DataTableProps<T = any> {
-  data: T[]
-  columns: DataTableColumn[]
-  limit?: number
-  searchBy?: string
-  sortKeys?: string[]
-  sortFunc?: (params: DataTableSortFunc) => number
-  searchFunc?: (params: DataTableSearchFunc) => unknown[]
-  filters?: DataTableFilter[]
-  mobileColumns?: string[]
-  className?: string
+  data: T[];
+  columns: DataTableColumn[];
+  limit?: number;
+  searchBy?: string;
+  sortKeys?: string[];
+  sortFunc?: (params: DataTableSortFunc) => number;
+  searchFunc?: (params: DataTableSearchFunc) => unknown[];
+  filters?: DataTableFilter[];
+  mobileColumns?: string[];
+  className?: string;
 }
 
 const DataTable = ({
   data = [],
   columns = [],
   limit = 20,
-  searchBy = '',
+  searchBy = "",
   searchFunc = defaultSearchFunc,
   sortKeys = [],
   filters = [],
   mobileColumns = [columns[0].accessor],
-  className
+  className,
 }: DataTableProps) => {
-  const [page, setPage] = useState(1)
-  const [search, setSearch] = useState('')
-  const [sortBy, setSortBy] = useState<string>()
-  const [sortType, setSortType] = useState<Sort>(Sort.asc)
-  const [appliedFilters, setAppliedFilters] = useState<DataTableAppliedFilter[]>([])
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState<string>();
+  const [sortType, setSortType] = useState<Sort>(Sort.asc);
+  const [appliedFilters, setAppliedFilters] = useState<
+    DataTableAppliedFilter[]
+  >([]);
 
   const filteredData = useMemo(() => {
-    let result = [...data]
+    let result = [...data];
     if (searchBy && searchFunc && search.trim()) {
-      result = result.filter(item => searchFunc({ item, field: searchBy, query: search.trim() }))
+      result = result.filter((item) =>
+        searchFunc({ item, field: searchBy, query: search.trim() })
+      );
     }
 
     if (sortBy) {
-      result.sort((itemA, itemB) => defaultSortFunc({ itemA, itemB, field: sortBy, direction: sortType }))
+      result.sort((itemA, itemB) =>
+        defaultSortFunc({ itemA, itemB, field: sortBy, direction: sortType })
+      );
     }
 
     if (appliedFilters) {
       for (const filter of appliedFilters) {
-        const filterConfig = filters.find(_filter => _filter.accessor === filter.accessor)
+        const filterConfig = filters.find(
+          (_filter) => _filter.accessor === filter.accessor
+        );
 
         if (!filterConfig) {
-          continue
+          continue;
         }
 
         switch (filterConfig.type) {
           case "select":
-            result = result.filter(item => defaultSelectFilter({ item, field: filterConfig.accessor, value: filter.value }))
-            break
-          case 'checkboxList':
-            result = result.filter(item => defaultCheckboxesFilter({ item, field: filterConfig.accessor, value: filter.value }))
-            break
+            result = result.filter((item) =>
+              defaultSelectFilter({
+                item,
+                field: filterConfig.accessor,
+                value: filter.value,
+              })
+            );
+            break;
+          case "checkboxList":
+            result = result.filter((item) =>
+              defaultCheckboxesFilter({
+                item,
+                field: filterConfig.accessor,
+                value: filter.value,
+              })
+            );
+            break;
         }
       }
     }
 
-    return result
-  }, [data, search, searchFunc, searchBy, sortBy, sortType, appliedFilters, filters])
-
-  const isHeaderVisible = sortKeys.length || filters.length || searchBy
-
-  return <DataTableContext.Provider value={{
-    data: filteredData,
-    initialData: data,
-    columns,
-    page,
-    setPage,
-    limit,
-    searchBy,
+    return result;
+  }, [
+    data,
     search,
-    setSearch,
-    sortKeys,
+    searchFunc,
+    searchBy,
     sortBy,
-    setSortBy,
     sortType,
-    setSortType,
-    filters,
     appliedFilters,
-    setAppliedFilters,
-    mobileColumns
-  }}>
-    <table className={clsx('alt-data-table', className)} data-testid='alt-test-datatable'>
-      <thead>
-        { isHeaderVisible && <DataTableHeader /> }
-        <DataTableHeaderRow />
-      </thead>
-      <DataTableBody />
-      <DataTableFooter />
-    </table>
-  </DataTableContext.Provider>
-}
+    filters,
+  ]);
 
-export default memo(DataTable)
+  const isHeaderVisible = sortKeys.length || filters.length || searchBy;
+
+  return (
+    <DataTableContext.Provider
+      value={{
+        data: filteredData,
+        initialData: data,
+        columns,
+        page,
+        setPage,
+        limit,
+        searchBy,
+        search,
+        setSearch,
+        sortKeys,
+        sortBy,
+        setSortBy,
+        sortType,
+        setSortType,
+        filters,
+        appliedFilters,
+        setAppliedFilters,
+        mobileColumns,
+      }}
+    >
+      <table
+        className={clsx("alt-data-table", className)}
+        data-testid="alt-test-datatable"
+      >
+        <thead>
+          {isHeaderVisible && <DataTableHeader />}
+          <DataTableHeaderRow />
+        </thead>
+        <DataTableBody />
+        <DataTableFooter />
+      </table>
+    </DataTableContext.Provider>
+  );
+};
+
+export default memo(DataTable);
