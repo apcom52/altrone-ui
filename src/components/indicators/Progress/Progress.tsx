926b7de77553a0950d440481e3ebf0d0f13978ae
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2023-01-18 10:25:51 +0300

    feat(2303): implemented circular progress

--- src/components/indicators/Progress/Progress.tsx
+++ src/components/indicators/Progress/Progress.tsx
@@ -6,7 +6,8 @@ import { ProgressSegment, ProgressSegmentProps } from './ProgressSegment';
 
 export enum ProgressVariant {
   default = 'default',
-  segmented = 'segmented'
+  segmented = 'segmented',
+  circular = 'circular'
 }
 
 interface ProgressProps {
@@ -19,6 +20,12 @@ interface ProgressProps {
   ProgressSegmentComponent?: React.FC<ProgressSegmentProps>;
 }
 
+const CIRCULAR_PROGRESS_DIAMETERS: Record<Size, number> = {
+  [Size.small]: 26,
+  [Size.medium]: 34,
+  [Size.large]: 42
+};
+
 const Progress = ({
   variant = ProgressVariant.default,
   value = 0,
@@ -30,12 +37,20 @@ const Progress = ({
 }: ProgressProps) => {
   const percent = Math.round((value / max) * 100);
 
+  const boxSize = CIRCULAR_PROGRESS_DIAMETERS[size];
+  const borderSize = Math.floor(CIRCULAR_PROGRESS_DIAMETERS[size] * 0.12);
+  const circleCenter = boxSize / 2;
+  const diameter = boxSize - borderSize * 2;
+  const radius = diameter / 2;
+  const circleValue = 2 * Math.PI * radius * (value / max);
+
   return (
     <div
       className={clsx('alt-progress', className, {
         'alt-progress--size-small': size === Size.small,
         'alt-progress--size-large': size === Size.large,
         'alt-progress--variant-segmented': variant === ProgressVariant.segmented,
+        'alt-progress--variant-circular': variant === ProgressVariant.circular,
         'alt-progress--primary': role === Role.primary,
         'alt-progress--success': role === Role.success,
         'alt-progress--danger': role === Role.danger
@@ -58,6 +73,25 @@ const Progress = ({
             />
           );
         })}
+      {variant === ProgressVariant.circular && (
+        <svg width={boxSize} height={boxSize} viewBox={`0 0 ${boxSize} ${boxSize}`}>
+          <circle
+            cx={circleCenter}
+            cy={circleCenter}
+            r={radius}
+            strokeWidth={borderSize}
+            className="alt-progress__circle"
+          />
+          <circle
+            cx={circleCenter}
+            cy={circleCenter}
+            r={radius}
+            strokeWidth={borderSize}
+            className="alt-progress__circle alt-progress__circle--active"
+            strokeDasharray={`${circleValue} 360`}
+          />
+        </svg>
+      )}
     </div>
   );
 };
