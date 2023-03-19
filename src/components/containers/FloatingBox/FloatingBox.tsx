fc6ca8de974c96b904e83b64d998975d0f05ddf6
apcom <apcom@live.ru>
2023-03-19 13:59:20 +0300

    types (2311): fixed FloatingBox type

--- src/components/containers/FloatingBox/FloatingBox.tsx
+++ src/components/containers/FloatingBox/FloatingBox.tsx
@@ -3,7 +3,7 @@ import { forwardRef, useMemo, useState } from 'react';
 import { usePopper } from 'react-popper';
 import './floating-box.scss';
 import { useOutsideClick } from 'rooks';
-import { Options } from '@popperjs/core';
+import { Options, State } from '@popperjs/core';
 import { createPortal } from 'react-dom';
 import { useWindowSize } from '../../../hooks';
 import { Modal } from '../Modal';
@@ -30,15 +30,18 @@ interface FloatingBoxProps extends WithoutDefaultOffsets {
   className?: string;
 }
 
-const setPopperWidth = (state, minWidth) => {
-  const targetRefWidth = state.elements.reference.clientWidth;
+const setPopperWidth = (state: State, minWidth: FloatingBoxProps['minWidth']) => {
+  const targetRefWidth = state.elements.reference.getBoundingClientRect().width;
 
   state.elements.popper.style.width = `${
     minWidth ? (targetRefWidth < minWidth ? minWidth : targetRefWidth) : targetRefWidth
   }px`;
 };
 
-const FloatingBox = forwardRef<HTMLDivElement, FloatingBoxProps>(
+const FloatingBox = forwardRef<
+  HTMLDivElement & { modifiers?: Options['modifiers'] },
+  FloatingBoxProps
+>(
   (
     {
       targetElement,
@@ -74,8 +77,8 @@ const FloatingBox = forwardRef<HTMLDivElement, FloatingBoxProps>(
       return [0, offset];
     }, [offset]);
 
-    const modifiers = useMemo(() => {
-      const result = [
+    const modifiers = useMemo<Options['modifiers']>(() => {
+      const result: Options['modifiers'] = [
         {
           name: 'offset',
           options: {
@@ -99,8 +102,11 @@ const FloatingBox = forwardRef<HTMLDivElement, FloatingBoxProps>(
         });
       }
 
-      if (ref) {
-        ref.modifiers = result;
+      if (ref && typeof ref !== 'function' && ref.current) {
+        ref.current = {
+          ...ref.current,
+          modifiers: result
+        };
       }
 
       return result;
@@ -160,5 +166,7 @@ const FloatingBox = forwardRef<HTMLDivElement, FloatingBoxProps>(
     );
   }
 );
+
+FloatingBox.displayName = 'FloatingBox';
 
 export default FloatingBox;
