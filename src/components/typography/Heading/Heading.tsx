e965077c2e5c13c0138542f4a5499e834013ef0f
Alexander Perevezentsev <apcom@live.ru>
2023-03-24 12:25:13 +0300

    refactor (2312): replaced Box with native tags in Heading

--- src/components/typography/Heading/Heading.tsx
+++ src/components/typography/Heading/Heading.tsx
@@ -1,6 +1,5 @@
 import { WithAltroneOffsets, WithoutDefaultOffsets } from '../../../types';
-import { memo } from 'react';
-import { Box } from '../../containers/Box';
+import React, { memo } from 'react';
 import './heading.scss';
 import clsx from 'clsx';
 
@@ -9,16 +8,15 @@ interface HeadingProps extends WithoutDefaultOffsets, WithAltroneOffsets {
 }
 
 const Heading = ({ children, level = 1, ...props }: HeadingProps) => {
-  const tagName = `h${level >= 1 && level <= 6 ? level : 1}` as keyof JSX.IntrinsicElements;
+  const HeadingTag = `h${level >= 1 && level <= 6 ? level : 1}`;
 
-  return (
-    <Box
-      tagName={tagName}
-      className={clsx('alt-heading', `alt-heading--level-${level}`, props.className)}
-      {...props}
-    >
-      {children}
-    </Box>
+  return React.createElement(
+    HeadingTag,
+    {
+      className: clsx('alt-heading', `alt-heading--level-${level}`, props.className),
+      ...props
+    },
+    children
   );
 };
 
