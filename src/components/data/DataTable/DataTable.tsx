3d85db470973242d7b3ac93577b5c5cadb7f0aef
Alexander Perevezentsev <apcom@live.ru>
2023-03-28 12:30:11 +0300

    fix (2313): fixed issue with hidden header if passed only selectable or actions

--- src/components/data/DataTable/DataTable.tsx
+++ src/components/data/DataTable/DataTable.tsx
@@ -151,7 +151,8 @@ export const DataTable = <T extends object>({
     }
   }, [selectableMode]);
 
-  const isHeaderVisible = sortKeys.length || filters.length || searchBy || selectable || actions?.length;
+  const isHeaderVisible =
+    sortKeys.length || filters.length || searchBy || selectable || actions?.length;
 
   return (
     <DataTableContext.Provider
