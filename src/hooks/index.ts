d918c7adf957c7cfa447e0e1664096e0e8453525
Alexander Perevezentsev <apcom@live.ru>
2023-04-08 09:34:37 +0300

    fixed issue with inability to use ultron in ssr due to missing window object

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { useResizeObserver } from './useResizeObserver';
 export { useWindowSize } from './useWindowSize';
 export { useLocalization } from './useLocalization';
+export { useMediaMatch } from './useMediaMatch';
 export { useOffset, useMargin, usePadding } from './useOffset';
 export type { Padding, Margin, OffsetObject } from './useOffset';
---
+++ src/hooks/useMediaMatch/index.ts
@@ -0,0 +1,1 @@
+export { useMediaMatch } from './useMediaMatch';
