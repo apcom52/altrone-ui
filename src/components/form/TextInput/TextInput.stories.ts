955cfa4df780d8f1a842ee150960b18f7d7f6cad
Alexander Perevezentsev <apcom@live.ru>
2023-04-09 09:40:26 +0300

    feat (2314): live suggestions

--- src/components/form/TextInput/TextInput.stories.ts
+++ src/components/form/TextInput/TextInput.stories.ts
@@ -5,6 +5,7 @@ export { default as LeftTextIsland } from './stories/LeftIslandWithText';
 export { default as RightIconIsland } from './stories/RightIslandWithIcon';
 export { default as BothIslands } from './stories/BothIslands';
 export { default as Suggestions } from './stories/TextInputSuggestions';
+export { default as LiveSuggestions } from './stories/TextInputLiveSuggestions';
 
 export default {
   component: TextInput,
---
+++ src/components/form/TextInput/stories/TextInputLiveSuggestions.tsx
@@ -0,0 +1,49 @@
+import { ComponentStory } from '@storybook/react';
+import { TextInput } from '../index';
+import { useState } from 'react';
+import { StorybookPlayground } from '../../../../storybook/StorybookPlayground';
+import { default as COUNTRIES } from '../../../data/DataTable/stories/data';
+import { Form, FormField, FormGroup } from '../../../containers';
+import { Switcher } from '../../Switcher';
+
+const SUGGESTIONS = COUNTRIES.map((country) => country.name);
+
+const TextInputLiveSuggestions: ComponentStory<typeof TextInput> = ({ placeholder = '' }) => {
+  const [value, setValue] = useState('');
+  const [isLeftActive, setIsLeftActive] = useState(true);
+  const [isRightActive, setIsRightActive] = useState(true);
+
+  return (
+    <StorybookPlayground>
+      <Form>
+        <FormGroup>
+          <FormField>
+            <Switcher checked={isLeftActive} onChange={setIsLeftActive}>
+              Show left island
+            </Switcher>
+          </FormField>
+          <FormField>
+            <Switcher checked={isRightActive} onChange={setIsRightActive}>
+              Show right island
+            </Switcher>
+          </FormField>
+        </FormGroup>
+      </Form>
+      <TextInput
+        value={value}
+        onChange={setValue}
+        placeholder={placeholder}
+        suggestions={SUGGESTIONS}
+        prefix={isLeftActive ? 'left' : undefined}
+        suffix={isRightActive ? 'right' : undefined}
+        useLiveSuggestions
+      />
+    </StorybookPlayground>
+  );
+};
+
+TextInputLiveSuggestions.args = {
+  placeholder: ''
+};
+
+export default TextInputLiveSuggestions;
