98bb9c7fa74a4f46cad275879997ba316aee52fe
Alexander Perevezentsev <apcom@live.ru>
2023-04-12 22:28:00 +0300

    feat (2315): add clear button for Search

--- src/components/form/Search/Search.tsx
+++ src/components/form/Search/Search.tsx
@@ -1,15 +1,39 @@
-import { TextInput, TextInputProps } from '../TextInput';
+import { InputIslandType, TextInput, TextInputProps } from '../TextInput';
 import { useLocalization } from '../../../hooks';
 import React from 'react';
 import { Icon } from '../../icons';
 import './search.scss';
 import clsx from 'clsx';
 
-export const Search = ({ value, placeholder = '', className, ...props }: TextInputProps) => {
+export const Search = ({
+  value,
+  onChange,
+  placeholder = '',
+  className,
+  ...props
+}: TextInputProps) => {
   const t = useLocalization();
 
   return (
-    <TextInput value={value} className={clsx('alt-search', className)} {...props}>
+    <TextInput
+      value={value}
+      onChange={onChange}
+      className={clsx('alt-search', className)}
+      {...props}
+      rightIsland={
+        value
+          ? {
+              type: InputIslandType.actions,
+              content: [
+                {
+                  icon: <Icon i="backspace" />,
+                  title: t('common.clear'),
+                  onClick: () => onChange('')
+                }
+              ]
+            }
+          : undefined
+      }>
       {!value && (
         <div className="alt-search-placeholder">
           <span className="alt-search-placeholder__icon">
