f1f13668ee590ca13b1ab66f1b149d528f1165ea
Alexander Perevezentsev <apcom@live.ru>
2023-04-16 13:56:39 +0300

    feat (2315): added new prop loading for TextInput

--- src/components/form/TextInput/stories/DefaultTextInput.tsx
+++ src/components/form/TextInput/stories/DefaultTextInput.tsx
@@ -3,18 +3,24 @@ import { TextInput } from '../index';
 import { useState } from 'react';
 import { StorybookPlayground } from '../../../../storybook/StorybookPlayground';
 
-const DefaultTextInputStory: ComponentStory<typeof TextInput> = ({ placeholder = '' }) => {
-  const [value, setValue] = useState('');
+const DefaultTextInputStory: ComponentStory<typeof TextInput> = ({
+  placeholder = '',
+  value,
+  onChange,
+  ...args
+}) => {
+  const [_value, setValue] = useState('');
 
   return (
     <StorybookPlayground>
-      <TextInput value={value} onChange={setValue} placeholder={placeholder} />
+      <TextInput value={_value} onChange={setValue} placeholder={placeholder} {...args} />
     </StorybookPlayground>
   );
 };
 
 DefaultTextInputStory.args = {
-  placeholder: ''
+  placeholder: '',
+  loading: false
 };
 
 export default DefaultTextInputStory;
