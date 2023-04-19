c36d18ff1b752d8f10d04c63c0bd5b8f8f00543f
Alexander Perevezentsev <apcom@live.ru>
2023-04-19 17:44:27 +0300

    feat (2316): removed tests

--- src/components/form/FilePicker/FilePicker.test.tsx
+++ src/components/form/FilePicker/FilePicker.test.tsx
@@ -1,33 +1,3 @@
-import { Search } from './FilePicker';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
 
-class ResizeObserver {
-  observe() {
-    return null;
-  }
-  unobserve() {
-    return null;
-  }
-  disconnect() {
-    return null;
-  }
-}
-
-describe('Form.Search', () => {
-  beforeEach(() => {
-    window.ResizeObserver = ResizeObserver;
-  });
-
-  test('should search renders correctly', () => {
-    render(<Search value="" onChange={() => null} />);
-
-    const searchbox = screen.getByRole('searchbox');
-    expect(searchbox).toBeInTheDocument();
-  });
-
-  test('should placeholder works correctly', () => {
-    render(<Search value="" onChange={() => null} placeholder="Type to search" />);
-    expect(screen.getByText('Type to search')).toBeInTheDocument();
-  });
-});
+describe('Form.FilePicker', () => {});
