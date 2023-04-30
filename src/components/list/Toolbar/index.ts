46aa159252f1eb285351be545ff907d4054ca444
Alexander Perevezentsev <apcom@live.ru>
2023-04-30 14:13:31 +0300

    fix (2317): fixed issue with absent of importing of ToolbarAction

--- src/components/list/Toolbar/index.ts
+++ src/components/list/Toolbar/index.ts
@@ -1,3 +1,4 @@
 export { default as Toolbar } from './Toolbar';
 export { default as ToolbarGroup } from './ToolbarGroup';
 export { default as ToolbarSeparator } from './ToolbarSeparator';
+export { default as ToolbarAction } from './ToolbarAction';
--- src/components/list/index.ts
+++ src/components/list/index.ts
@@ -1,5 +1,5 @@
 export { ContextMenu } from './ContextMenu';
 export { Chips } from './Chips';
 export { TabList, TabListVariant } from './TabList';
-export { Toolbar, ToolbarGroup, ToolbarSeparator } from './Toolbar';
+export { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarAction } from './Toolbar';
 export { NavigationList, NAVIGATION_LIST_SEPARATOR } from './NavigationList';
