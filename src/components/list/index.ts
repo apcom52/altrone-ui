267c6d34982b1220ad43e6967e739dc4cc2ebeca
Alexander Perevezentsev <apcom@live.ru>
2023-05-11 10:35:43 +0300

    feat (2319): added import of Breadcrumbs

--- src/components/list/index.ts
+++ src/components/list/index.ts
@@ -3,3 +3,4 @@ export { Chips } from './Chips';
 export { TabList, TabListVariant } from './TabList';
 export { Toolbar, ToolbarGroup, ToolbarSeparator } from './Toolbar';
 export { NavigationList, NAVIGATION_LIST_SEPARATOR } from './NavigationList';
+export { Breadcrumbs, BreadcrumbLink } from './Breadcrumbs';
