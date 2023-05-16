ef9b2a840d6148c1278da1b3ad48b3dcaa734618
Alexander Perevezentsev <apcom@live.ru>
2023-05-16 11:09:22 +0300

    feat (2320): navigation feature for Pagination

--- src/components/indicators/Pagination/Pagination.tsx
+++ src/components/indicators/Pagination/Pagination.tsx
@@ -1,7 +1,10 @@
 import './pagination.scss';
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Button, ButtonVariant } from '../../button';
 import { Icon } from '../../icons';
+import { FloatingBox, Form, FormField } from '../../containers';
+import { NumberInput } from '../../form';
+import { Role } from '../../../types';
 
 interface PaginationProps {
   page: number;
@@ -16,6 +19,10 @@ export const Pagination = ({
   onChange,
   useNavigateToPagePopup = true
 }: PaginationProps) => {
+  const [isNavigationVisible, setIsNavigationVisible] = useState(false);
+
+  const navigationButtonRef = useRef<HTMLButtonElement>(null);
+
   const isPrevDisabled = page === 1;
   const isNextDisabled = page === totalPages;
 
@@ -31,17 +38,40 @@ export const Pagination = ({
     }
   }, [page, totalPages, onChange]);
 
+  const closeNavigation = useCallback(() => {
+    setIsNavigationVisible(false);
+  }, []);
+
   return (
     <div className="alt-pagination">
       <Button variant={ButtonVariant.transparent} disabled={isPrevDisabled} onClick={prev} isIcon>
         <Icon i="arrow_back_ios_new" />
       </Button>
-      <button disabled={!useNavigateToPagePopup} className="alt-pagination__state">
+      <button
+        disabled={!useNavigateToPagePopup}
+        onClick={() => setIsNavigationVisible(true)}
+        className="alt-pagination__state"
+        ref={navigationButtonRef}>
         {page} / {totalPages}
       </button>
       <Button variant={ButtonVariant.transparent} disabled={isNextDisabled} onClick={next} isIcon>
         <Icon i="arrow_forward_ios" />
       </Button>
+      {isNavigationVisible && (
+        <FloatingBox
+          targetElement={navigationButtonRef.current}
+          onClose={closeNavigation}
+          useRootContainer>
+          <Form>
+            <FormField label="Перейти к странице">
+              <NumberInput value={page} min={1} max={totalPages} onChange={onChange} />
+            </FormField>
+            <Button role={Role.primary} fluid onClick={closeNavigation}>
+              Перейти
+            </Button>
+          </Form>
+        </FloatingBox>
+      )}
     </div>
   );
 };
