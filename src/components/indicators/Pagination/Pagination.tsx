5e62abc0626e2bfc93a1ae373f7a1e54ee31b329
Alexander Perevezentsev <apcom@live.ru>
2023-05-16 23:31:21 +0300

    Feat/2320 pagination (#163)
    * feat (2320): basic pagination
    
    * feat (2320): navigation feature for Pagination
    
    * docs (2320): pagination documentation
    
    * refactor (2320): applied Pagination for DataTable
    
    * test (2320): updated tests for DataTable

---
+++ src/components/indicators/Pagination/Pagination.tsx
@@ -0,0 +1,87 @@
+import './pagination.scss';
+import React, { useCallback, useRef, useState } from 'react';
+import { Button, ButtonVariant } from '../../button';
+import { Icon } from '../../icons';
+import { FloatingBox, Form, FormField } from '../../containers';
+import { NumberInput } from '../../form';
+import { Role } from '../../../types';
+
+interface PaginationProps {
+  page: number;
+  totalPages: number;
+  onChange: (page: number) => void;
+  useNavigateToPagePopup?: boolean;
+}
+
+/**
+ * This component indicates the current page of the list
+ * @param page current page
+ * @param totalPages total amount of pages
+ * @param onChange callback fires when user changes the current page
+ * @param useNavigateToPagePopup when this prop is activated pagination component has the ability to move to the specific page
+ * @constructor
+ */
+export const Pagination = ({
+  page = 1,
+  totalPages = 1,
+  onChange,
+  useNavigateToPagePopup = true
+}: PaginationProps) => {
+  const [isNavigationVisible, setIsNavigationVisible] = useState(false);
+
+  const navigationButtonRef = useRef<HTMLButtonElement>(null);
+
+  const isPrevDisabled = page === 1;
+  const isNextDisabled = page === totalPages;
+
+  const prev = useCallback(() => {
+    if (page > 0) {
+      onChange(page - 1);
+    }
+  }, [page, onChange]);
+
+  const next = useCallback(() => {
+    if (page < totalPages) {
+      onChange(page + 1);
+    }
+  }, [page, totalPages, onChange]);
+
+  const closeNavigation = useCallback(() => {
+    setIsNavigationVisible(false);
+  }, []);
+
+  return (
+    <div className="alt-pagination">
+      <Button variant={ButtonVariant.transparent} disabled={isPrevDisabled} onClick={prev} isIcon>
+        <Icon i="arrow_back_ios_new" />
+      </Button>
+      <button
+        disabled={!useNavigateToPagePopup}
+        onClick={() => setIsNavigationVisible(true)}
+        className="alt-pagination__state"
+        ref={navigationButtonRef}>
+        <span data-testid="alt-test-pagination-currentPage">{page}</span> / {totalPages}
+      </button>
+      <Button variant={ButtonVariant.transparent} disabled={isNextDisabled} onClick={next} isIcon>
+        <Icon i="arrow_forward_ios" />
+      </Button>
+      {isNavigationVisible && (
+        <FloatingBox
+          targetElement={navigationButtonRef.current}
+          onClose={closeNavigation}
+          className="alt-pagination__navigation"
+          placement="top"
+          useRootContainer>
+          <Form>
+            <FormField label="Перейти к странице">
+              <NumberInput value={page} min={1} max={totalPages} onChange={onChange} />
+            </FormField>
+            <Button role={Role.primary} fluid onClick={closeNavigation}>
+              Перейти
+            </Button>
+          </Form>
+        </FloatingBox>
+      )}
+    </div>
+  );
+};
