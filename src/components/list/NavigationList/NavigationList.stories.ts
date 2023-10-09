7cc28aebb1f305c34e925622222ef7164a676adf
Alexander Perevezentsev <apcom@live.ru>
2023-10-09 12:21:35 +0300

    docs(NavigationList): updated story

--- src/components/list/NavigationList/NavigationList.stories.ts
+++ src/components/list/NavigationList/NavigationList.stories.ts
@@ -10,7 +10,20 @@ const meta: Meta<typeof NavigationList> = {
   args: {
     title: 'Settings'
   },
-  argTypes: {}
+  argTypes: {
+    list: { description: 'Structure of the navigation' },
+    selected: { description: 'Selected item' },
+    onChange: { description: 'Callback is called when user selects new list item' },
+    title: { description: 'Title of the menu' },
+    action: { description: 'Action in the top right corner of the list' },
+    compact: { description: 'Makes the list compact' },
+    NavigationItemComponent: { description: 'Custom top level item component' },
+    NavigationSubItemComponent: { description: 'Custom second level item component' },
+    NavigationSubSubItemComponent: { description: 'Custom third level item component' },
+    surface: { description: 'Surface of the list' },
+    elevation: { description: 'Shadows of the list' },
+    className: { description: 'Custom CSS class' }
+  }
 };
 
 export default meta;
