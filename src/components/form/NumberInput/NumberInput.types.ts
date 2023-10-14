b909dfc2fed7bb4fbda328170b7842e4f084924e
Alexander Perevezentsev <apcom@live.ru>
2023-10-14 20:25:35 +0300

    feat(NumberInput): added thousandSeparator prop

---
+++ src/components/form/NumberInput/NumberInput.types.ts
@@ -0,0 +1,27 @@
+import { TextInputProps } from '../TextInput';
+
+export interface NumberInputProps
+  extends Omit<
+    TextInputProps,
+    | 'value'
+    | 'onChange'
+    | 'step'
+    | 'min'
+    | 'max'
+    | 'ref'
+    | 'suggestions'
+    | 'useLiveSuggestions'
+    | 'loading'
+  > {
+  value: number;
+  onChange: (value: number) => void;
+  showControls?: boolean;
+  allowNegative?: boolean;
+  allowLeadingZeros?: boolean;
+  decimalSeparator?: string;
+  thousandSeparator?: string;
+  digitsAfterDecimal?: number;
+  step?: number;
+  min?: number;
+  max?: number;
+}
