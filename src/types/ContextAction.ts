b6a460bfb91c2b62e26bc125f407f2ca4e35e14c
Alexander Perevezentsev <apcom@live.ru>
2023-11-06 17:33:07 +0300

    feat(ContextMenu): reworked types

--- src/types/ContextAction.ts
+++ src/types/ContextAction.ts
@@ -1,16 +1,42 @@
-export interface ContextAction {
+type BaseContextAction = {
   title: string;
-  onClick: (...args: unknown[]) => void;
   icon?: JSX.Element;
   hint?: string;
   disabled?: boolean;
-  danger?: boolean;
-  selected?: boolean;
-}
+};
 
-export interface ParentContextAction extends Omit<ContextAction, 'onClick' | 'hint' | 'danger'> {
+type ClickAction = {
+  type?: 'action';
+  onClick: (...args: unknown[]) => void;
+  selected?: boolean;
+  danger?: boolean;
+};
+
+type CheckboxAction = {
+  type: 'checkbox';
+  onChange: (state: boolean) => void;
+  checked: boolean;
+};
+
+type RadioListAction = {
+  type: 'radioList';
+  groupName: string;
+  value: unknown;
+  onChange: (selectedOption: unknown) => void;
+  selectedValue: unknown;
+};
+
+export type ContextClickAction = BaseContextAction & ClickAction;
+export type ContextCheckboxAction = BaseContextAction & CheckboxAction;
+export type ContextRadioListAction = BaseContextAction & RadioListAction;
+export type ContextAction = ContextClickAction | ContextCheckboxAction | ContextRadioListAction;
+
+export type ParentContextAction = {
+  title: string;
   children: ContextAction[];
-}
+  icon?: JSX.Element;
+  danger?: boolean;
+};
 
 export type ContextSeparator = '-';
 
