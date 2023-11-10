d79a0b752d42c1e2a03e2ae97a935553b3755272
Alexander Perevezentsev <apcom@live.ru>
2023-11-10 19:56:47 +0300

    fix(Spoiler): when spoiler is not opened it doesn't place his content in DOM tree

--- src/components/containers/Spoiler/Spoiler.tsx
+++ src/components/containers/Spoiler/Spoiler.tsx
@@ -3,6 +3,7 @@ import { Icon } from '../../typography';
 import './spoiler.scss';
 import clsx from 'clsx';
 import { motion } from 'framer-motion';
+import { useToggledState } from '../../../hooks';
 
 interface SpoilerProps extends PropsWithChildren {
   label: string;
@@ -17,29 +18,56 @@ interface SpoilerProps extends PropsWithChildren {
  * @constructor
  */
 const Spoiler = ({ label, openedByDefault = true, children }: SpoilerProps) => {
-  const [opened, setOpened] = useState(openedByDefault);
+  const {
+    value: opened,
+    enable: showSpoiler,
+    disable: hideSpoiler,
+    setValue: setOpened
+  } = useToggledState(openedByDefault);
+
+  const [height, setHeight] = useState(openedByDefault ? 'auto' : 0);
+
+  const onHeaderClick = () => {
+    if (!opened) {
+      showSpoiler();
+      setHeight('auto');
+    } else {
+      setHeight(0);
+    }
+  };
+
+  const onAnimationComplete = () => {
+    if (height === 0) {
+      hideSpoiler();
+    }
+  };
 
   useEffect(() => {
     setOpened(openedByDefault);
+    setHeight(openedByDefault ? 'auto' : 0);
   }, [openedByDefault]);
 
   return (
     <div
       className={clsx('alt-spoiler', {
-        'alt-spoiler--opened': opened
+        'alt-spoiler--opened': height === 'auto'
       })}>
-      <button className="alt-spoiler__header" onClick={() => setOpened(!opened)}>
+      <button className="alt-spoiler__header" onClick={onHeaderClick}>
         <span className="alt-spoiler__headerIcon">
           <Icon i="chevron_right" />
         </span>
         <span className="alt-spoiler__headerLabel">{label}</span>
       </button>
-      <motion.div
-        className="alt-spoiler__content"
-        initial={{ height: openedByDefault ? 'auto' : 0 }}
-        animate={{ height: opened ? 'auto' : 0 }}>
-        {children}
-      </motion.div>
+
+      {opened && (
+        <motion.div
+          className="alt-spoiler__content"
+          initial={{ height: 0 }}
+          animate={{ height }}
+          onAnimationComplete={onAnimationComplete}>
+          {children}
+        </motion.div>
+      )}
     </div>
   );
 };
