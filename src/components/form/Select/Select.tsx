aede78eb7b1fe4849d836d1a10200c22f319400d
Alexander Perevezentsev <apcom@live.ru>
2024-03-06 22:19:01 +0400

    feat(Select): added support of new Popover component

--- src/components/form/Select/Select.tsx
+++ src/components/form/Select/Select.tsx
@@ -1,6 +1,6 @@
-import { createElement, memo, useEffect, useMemo, useRef, useState } from 'react';
+import { createElement, useEffect, useMemo, useRef, useState } from 'react';
 import { Direction, Elevation, Option, OptionParent, Role, Size, Surface } from '../../../types';
-import { ButtonContainer, FloatingBox, FloatingBoxMobileBehaviour } from '../../containers';
+import { ButtonContainer, Popover } from '../../containers';
 import './select.scss';
 import { Icon } from '../../typography';
 import clsx from 'clsx';
@@ -8,7 +8,7 @@ import { TextInput } from '../TextInput';
 import SelectOption, { SelectOptionProps } from './SelectOption';
 import { useLocalization, useWindowSize } from '../../../hooks';
 import { ScrollableSelector } from '../ScrollableSelector';
-import { Button } from '../../form';
+import { Button, Search } from '../../form';
 import SelectPlaceholder from './SelectPlaceholder';
 import { BasicInput, BasicInputProps } from '../BasicInput';
 
@@ -79,9 +79,6 @@ const Select = <T extends unknown>({
   const { ltePhoneL, gtPhoneL } = useWindowSize();
   const t = useLocalization();
 
-  const [isSelectVisible, setIsSelectVisible] = useState(false);
-  const [isSearchMode, setIsSearchMode] = useState(false);
-
   const [searchTerm, setSearchTerm] = useState('');
 
   const selectRef = useRef<HTMLButtonElement>(null);
@@ -136,163 +133,122 @@ const Select = <T extends unknown>({
     return options.find((option) => option.value === value) || null;
   }, [value, options]);
 
-  const onSelectMenuClose = () => {
-    setIsSelectVisible(false);
-    setIsSearchMode(false);
-  };
-
-  const onSelectOption = (value: T) => {
+  const onSelectOption = (closePopup: () => void, value: T) => {
     onChange(value);
-    onSelectMenuClose();
+    closePopup();
   };
-
-  const onSelectClick = () => {
-    setTimeout(() => {
-      setIsSelectVisible(true);
-    }, 0);
-
-    if (searchable) {
-      setIsSearchMode(true);
-    }
-  };
-
-  const onInputBlur = () => {
-    if (!isSelectVisible) {
-      setIsSearchMode(false);
-    }
-  };
-
-  const preventSelectMenuClose = (e: MouseEvent) => {
-    return isSearchMode ? e.target === inputRef.current : e.target === selectRef.current;
-  };
-
-  useEffect(() => {
-    if (!isSearchMode) {
-      setSearchTerm('');
-    }
-  }, [isSearchMode]);
 
   return (
     <BasicInput hintText={hintText} errorText={errorText} disabled={disabled} size={size}>
-      {!searchable || (searchable && !isSearchMode) ? (
-        <button
-          ref={selectRef}
-          id={id}
-          disabled={disabled}
-          onClick={onSelectClick}
-          data-testid="alt-test-select"
-          className={clsx('alt-select', classNames.select, {
-            'alt-select--active': isSelectVisible,
-            'alt-select--disabled': disabled,
-            [`alt-select--elevation-${elevation}`]: elevation,
-            [`alt-select--surface-${surface}`]: surface !== Surface.paper
-          })}
-          type="button">
-          <div
-            className={clsx('alt-select__value', classNames.currentValue)}
-            data-testid="alt-test-select-current-value">
-            {selectedOption ? (
-              createElement(ItemComponent, {
-                label: selectedOption?.label,
-                value: selectedOption?.value,
-                selected: false,
-                disabled: false,
-                onSelect: () => null,
-                inSelectHeader: true
-              })
-            ) : (
-              <SelectPlaceholder>{placeholder || t('form.select.placeholder')}</SelectPlaceholder>
+      <Popover
+        placement="bottom"
+        trigger="click"
+        useParentWidth
+        content={({ closePopup }) => (
+          <>
+            {searchable && (
+              <Search value={searchTerm} onChange={setSearchTerm} className="alt-select-search" />
             )}
-          </div>
-          <div className="alt-select__arrow">
-            <Icon i="expand_more" />
-          </div>
-        </button>
-      ) : (
-        <TextInput
-          ref={inputRef}
-          placeholder={t('form.select.search')}
-          value={searchTerm}
-          onChange={setSearchTerm}
-          rightIcon={<Icon i="search" />}
-          onBlur={onInputBlur}
-          autoFocus
-        />
-      )}
-      {isSelectVisible && (
-        <FloatingBox
-          placement="bottom"
-          targetElement={isSearchMode ? inputRef.current : selectRef.current}
-          onClose={onSelectMenuClose}
-          minWidth={200}
-          preventClose={searchable && isSearchMode ? preventSelectMenuClose : undefined}
-          data-testid="alt-test-select-search"
-          useParentWidth
-          mobileBehaviour={FloatingBoxMobileBehaviour.modal}
-          useRootContainer={true}
-          closeOnAnotherFloatingBoxClick>
-          {gtPhoneL && (
-            <div
-              className={clsx('alt-select-menu', classNames.menu)}
-              data-testid="alt-test-select-menu">
-              {clearable && value ? (
-                <SelectOption<undefined>
-                  label="—"
-                  value={undefined}
-                  onSelect={() => onSelectOption(undefined as T)}
-                  disabled={false}
-                  selected={false}
-                />
-              ) : null}
-              {parentKeys.map((groupValue, groupIndex, groupedValueKeys) => {
-                const group = groupedItems[groupValue];
-                const [groupInfo, ...options] = group;
-                const isSingle = groupedValueKeys.length === 1;
-
-                return (
-                  <div className="alt-select-group" key={groupValue}>
-                    {!isSingle && options.length ? (
-                      <div className="alt-select-group__title">{groupInfo.label}</div>
-                    ) : null}
-                    {options.map((option, optionIndex) =>
-                      createElement(ItemComponent, {
-                        key: optionIndex,
-                        label: option.label,
-                        value: option.value,
-                        selected: option.value === value,
-                        disabled: option.disabled || groupInfo.disabled || false,
-                        onSelect: onSelectOption,
-                        inSelectHeader: false
-                      })
-                    )}
-                  </div>
-                );
-              })}
-            </div>
-          )}
-          {ltePhoneL && (
-            <>
-              <div className="alt-select-menu__title">{t('form.select.placeholder')}</div>
-              <ScrollableSelector<T>
-                className="alt-select-menu__selector"
-                options={options}
-                value={value}
-                onChange={onChange}
-              />
-              <ButtonContainer direction={Direction.vertical}>
+            {gtPhoneL && (
+              <div
+                className={clsx('alt-select-menu', classNames.menu)}
+                data-testid="alt-test-select-menu">
                 {clearable && value ? (
-                  <Button role={Role.default} onClick={() => onSelectOption(undefined as T)} fluid>
-                    {t('common.clear')}
-                  </Button>
+                  <SelectOption<undefined>
+                    label="—"
+                    value={undefined}
+                    onSelect={() => onSelectOption(closePopup, undefined as T)}
+                    disabled={false}
+                    selected={false}
+                  />
                 ) : null}
-                <Button role={Role.primary} onClick={() => setIsSelectVisible(false)} fluid>
-                  {t('common.apply')}
-                </Button>
-              </ButtonContainer>
-            </>
-          )}
-        </FloatingBox>
-      )}
+                {parentKeys.map((groupValue, groupIndex, groupedValueKeys) => {
+                  const group = groupedItems[groupValue];
+                  const [groupInfo, ...options] = group;
+                  const isSingle = groupedValueKeys.length === 1;
+
+                  return (
+                    <div className="alt-select-group" key={groupValue}>
+                      {!isSingle && options.length ? (
+                        <div className="alt-select-group__title">{groupInfo.label}</div>
+                      ) : null}
+                      {options.map((option, optionIndex) =>
+                        createElement(ItemComponent, {
+                          key: optionIndex,
+                          label: option.label,
+                          value: option.value,
+                          selected: option.value === value,
+                          disabled: option.disabled || groupInfo.disabled || false,
+                          onSelect: onSelectOption.bind(null, closePopup),
+                          inSelectHeader: false
+                        })
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+            {ltePhoneL && (
+              <>
+                <div className="alt-select-menu__title">{t('form.select.placeholder')}</div>
+                <ScrollableSelector<T>
+                  className="alt-select-menu__selector"
+                  options={options}
+                  value={value}
+                  onChange={onChange}
+                />
+                <ButtonContainer direction={Direction.vertical}>
+                  {clearable && value ? (
+                    <Button
+                      role={Role.default}
+                      onClick={() => onSelectOption(closePopup, undefined as T)}
+                      fluid>
+                      {t('common.clear')}
+                    </Button>
+                  ) : null}
+                  <Button role={Role.primary} onClick={closePopup} fluid>
+                    {t('common.apply')}
+                  </Button>
+                </ButtonContainer>
+              </>
+            )}
+          </>
+        )}>
+        {({ opened }) => (
+          <button
+            ref={selectRef}
+            id={id}
+            disabled={disabled}
+            data-testid="alt-test-select"
+            className={clsx('alt-select', classNames.select, {
+              'alt-select--active': opened,
+              'alt-select--disabled': disabled,
+              [`alt-select--elevation-${elevation}`]: elevation,
+              [`alt-select--surface-${surface}`]: surface !== Surface.paper
+            })}
+            type="button">
+            <div
+              className={clsx('alt-select__value', classNames.currentValue)}
+              data-testid="alt-test-select-current-value">
+              {selectedOption ? (
+                createElement(ItemComponent, {
+                  label: selectedOption?.label,
+                  value: selectedOption?.value,
+                  selected: false,
+                  disabled: false,
+                  onSelect: () => null,
+                  inSelectHeader: true
+                })
+              ) : (
+                <SelectPlaceholder>{placeholder || t('form.select.placeholder')}</SelectPlaceholder>
+              )}
+            </div>
+            <div className="alt-select__arrow">
+              <Icon i="expand_more" />
+            </div>
+          </button>
+        )}
+      </Popover>
     </BasicInput>
   );
 };
