18ddeb0717dda655a21d03b8530c7a2edf33ec81
Alexander Perevezentsev <apcom@live.ru>
2024-03-14 20:48:48 +0400

    refactor(Link T-150): removed old instances from NavigationList

--- src/components/list/NavigationList/NavigationList.tsx
+++ src/components/list/NavigationList/NavigationList.tsx
@@ -1,148 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './navigation-list.scss';
 import clsx from 'clsx';
 import { ContextMenuType, Elevation, Indicator, Surface } from '../../../types';
 import { NavigationListProps } from './NavigationList.types';
 import { NavigationDivider, NavigationHeader, NavigationLink, NavigationMenu } from './component';
 
-interface SubSubNavigationItem {
-  label: string;
-  value: unknown;
-  indicator?: Indicator;
-}
-
-interface SubNavigationItem {
-  label: string;
-  value: unknown;
-  icon?: JSX.Element;
-  submenu?: SubSubNavigationItem[];
-  indicator?: Indicator;
-}
-
-interface NavigationItem {
-  label: string;
-  value: unknown;
-  icon?: JSX.Element;
-  submenu?: SubNavigationItem[];
-  indicator?: Indicator;
-  compact?: boolean;
-}
-
-interface BaseNavigationItemInterface {
-  selected: boolean;
-  selectedValue: unknown;
-  onClick: (value: unknown) => void;
-  elevation?: Elevation;
-}
-
-interface NavigationListAction {
-  icon: JSX.Element;
-  title?: string;
-  disabled?: boolean;
-  danger?: boolean;
-  onClick?: () => void;
-  contextMenu?: ContextMenuType;
-}
-
-export interface NavigationItemProps extends BaseNavigationItemInterface, NavigationItem {}
-export interface NavigationSubItemProps extends BaseNavigationItemInterface, SubNavigationItem {}
-export interface NavigationSubSubItemProps
-  extends Omit<BaseNavigationItemInterface, 'onExpand'>,
-    SubSubNavigationItem {}
-
-export type NavigationListMenu = (NavigationItem | '-')[];
-
 /**
  * This component is used to make a navigation throughout your application
  * @param list
- * @param selected
- * @param onChange
  * @param className
  * @param surface
- * @param elevation
- * @param compact
  * @constructor
  */
-const NavigationListComponent = <SelectedPage = unknown,>({
+const NavigationListComponent = ({
   children,
-  selected,
-  onChange,
   className,
-  surface = Surface.glass,
-  elevation = Elevation.raised,
-  compact
-}: NavigationListProps<SelectedPage>) => {
-  const [compacted, setCompacted] = useState(compact === 'static');
-
-  // const [selectedItem, selectedSubItem, selectedSubSubItem] = useMemo(() => {
-  //   for (const item of list) {
-  //     if (item === NAVIGATION_LIST_SEPARATOR) {
-  //       continue;
-  //     }
-  //
-  //     if (item.value === selected) {
-  //       return [item.value, null, null];
-  //     }
-  //
-  //     if (item?.submenu?.length) {
-  //       for (const subitem of item.submenu) {
-  //         if (subitem.value === selected) {
-  //           return [item.value, subitem.value, null];
-  //         }
-  //
-  //         if (subitem?.submenu?.length) {
-  //           for (const subsubitem of subitem.submenu) {
-  //             if (subsubitem.value === selected) {
-  //               return [item.value, subitem.value, subsubitem.value];
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //
-  //   return [null, null, null];
-  // }, [selected, list]);
-
-  // useEffect(() => {
-  //   if (!selectedItem || selectedSubSubItem || !list.length) {
-  //     return;
-  //   }
-  //
-  //   if (selectedItem && !selectedSubItem) {
-  //     const firstLevelItem = list.find(
-  //       (item) => item !== NAVIGATION_LIST_SEPARATOR && item.value === selectedItem
-  //     ) as NavigationItem | undefined;
-  //
-  //     if (firstLevelItem && firstLevelItem.submenu?.length) {
-  //       onChange(firstLevelItem.submenu?.[0].value);
-  //       return;
-  //     }
-  //   }
-  //
-  //   if (selectedItem && selectedSubItem && !selectedSubSubItem) {
-  //     const firstLevelItem = list.find(
-  //       (item) => item !== NAVIGATION_LIST_SEPARATOR && item.value === selectedItem
-  //     ) as NavigationItem | undefined;
-  //
-  //     if (!firstLevelItem) {
-  //       return;
-  //     }
-  //     const secondLevelItem = firstLevelItem.submenu?.find(
-  //       (item) => item.value === selectedSubItem
-  //     );
-  //     if (secondLevelItem && secondLevelItem.submenu?.length) {
-  //       onChange(secondLevelItem.submenu?.[0].value);
-  //       return;
-  //     }
-  //   }
-  // }, [list, selectedItem, selectedSubItem, selectedSubSubItem, onChange]);
-
+  surface = Surface.glass
+}: NavigationListProps) => {
   return (
     <div
       className={clsx('alt-navigation-list', className, {
-        [`alt-navigation-list--surface-${surface}`]: surface !== Surface.glass,
-        'alt-navigation-list--compact': compact === 'static' || compacted
+        [`alt-navigation-list--surface-${surface}`]: surface !== Surface.glass
       })}>
       {children}
     </div>
--- src/components/list/NavigationList/NavigationList.types.ts
+++ src/components/list/NavigationList/NavigationList.types.ts
@@ -1,14 +1,10 @@
 import { Elevation, Indicator, SafeReactElement, Surface } from '../../../types';
 import { ReactElement } from 'react';
 
-export interface NavigationListProps<SelectedPage = unknown> {
+export interface NavigationListProps {
   children: SafeReactElement;
-  selected?: SelectedPage;
-  onChange?: (selectedValue: SelectedPage) => void;
   className?: string;
   surface?: Surface;
-  compact?: 'manual' | 'static';
-  elevation?: Elevation;
 }
 
 export interface NavigationHeaderProps {
