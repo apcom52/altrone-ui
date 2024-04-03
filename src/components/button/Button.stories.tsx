de94697c0e8983f4ddb52c4f402da23f6a64743d
Alexander Perevezentsev <apcom@live.ru>
2024-04-03 10:57:17 +0400

    (Link T-405): implemented sizes of Button

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Button, Flex, Icon, Text, TextHeadingRoles } from 'components';
-import { Align, Direction, Gap, Role } from 'types';
+import { Align, Direction, Gap, Role, Size } from 'types';
 import { StorybookDecorator } from 'global/storybook';
 import { allModes } from '../../../.storybook/modes.ts';
 import { roleStoryField } from '../../global/storybook/argTypes.ts';
@@ -12,14 +12,6 @@ const story: Meta<typeof Button> = {
   args: {},
   argTypes: {
     role: roleStoryField,
-  },
-  parameters: {
-    chromatic: {
-      modes: {
-        light: allModes['light desktop'],
-        dark: allModes['dark desktop'],
-      },
-    },
   },
 };
 
@@ -50,8 +42,27 @@ const renderButtonsWithRole = (role: Role) => {
   );
 };
 
+const renderButtonsWithSize = (size: Size) => {
+  return (
+    <Flex gap={Gap.medium} align={Align.start} direction={Direction.horizontal}>
+      <Button size={size} label="Like" leftIcon={<Icon i="favorite" />} />
+      <Button size={size} label="Like" rightIcon={<Icon i="favorite" />} />
+      <Button size={size} label="Like" />
+      <Button size={size} leftIcon={<Icon i="favorite" />} />
+    </Flex>
+  );
+};
+
 export const ButtonStory: StoryObj<typeof Button> = {
   name: 'Using Buttons',
+  parameters: {
+    chromatic: {
+      modes: {
+        light: allModes['light desktop'],
+        dark: allModes['dark desktop'],
+      },
+    },
+  },
   render: () => (
     <Flex gap={Gap.large}>
       <Text.Heading role={TextHeadingRoles.inner}>Default buttons</Text.Heading>
@@ -64,6 +75,27 @@ export const ButtonStory: StoryObj<typeof Button> = {
       {renderButtonsWithRole(Role.warning)}
       <Text.Heading role={TextHeadingRoles.inner}>Warning buttons</Text.Heading>
       {renderButtonsWithRole(Role.danger)}
+    </Flex>
+  ),
+};
+
+export const ButtonSizeStory: StoryObj<typeof Button> = {
+  name: 'Different sizes of buttons',
+  parameters: {
+    chromatic: {
+      modes: {
+        light: allModes['light desktop'],
+      },
+    },
+  },
+  render: () => (
+    <Flex gap={Gap.large}>
+      <Text.Heading role={TextHeadingRoles.inner}>Small buttons</Text.Heading>
+      {renderButtonsWithSize(Size.small)}
+      <Text.Heading role={TextHeadingRoles.inner}>Normal buttons</Text.Heading>
+      {renderButtonsWithSize(Size.medium)}
+      <Text.Heading role={TextHeadingRoles.inner}>Large buttons</Text.Heading>
+      {renderButtonsWithSize(Size.large)}
     </Flex>
   ),
 };
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,8 +1,8 @@
 import { ButtonProps } from './Button.types.ts';
-import { Flex } from '../flex';
+import { Flex } from 'components';
 import s from './button.module.scss';
 import clsx from 'clsx';
-import { Direction, Gap, Role } from '../../types';
+import { Align, Direction, Gap, Role, Size } from 'types';
 
 export const Button = ({
   label,
@@ -10,9 +10,12 @@ export const Button = ({
   rightIcon,
   transparent,
   role = Role.default,
+  size = Size.medium,
   className,
   ...props
 }: ButtonProps) => {
+  const isOnlyIcon = Boolean(!label && (leftIcon || rightIcon));
+
   const cls = clsx(
     s.Button,
     {
@@ -21,18 +24,25 @@ export const Button = ({
       [s.Success]: role === Role.success,
       [s.Warning]: role === Role.warning,
       [s.Danger]: role === Role.danger,
+      [s.Small]: size === Size.small,
+      [s.Large]: size === Size.large,
+      [s.OnlyIcon]: isOnlyIcon,
     },
     className,
   );
 
-  const buttonContent = label ? (
-    <Flex direction={Direction.horizontal} gap={Gap.small}>
+  const buttonContent = !isOnlyIcon ? (
+    <Flex
+      direction={Direction.horizontal}
+      gap={size === Size.large ? Gap.small : Gap.xsmall}
+      align={Align.center}
+    >
       {leftIcon ? <div className={s.Icon}>{leftIcon}</div> : null}
       <div className={s.Label}>{label}</div>
       {rightIcon ? <div className={s.Icon}>{rightIcon}</div> : null}
     </Flex>
   ) : (
-    leftIcon || rightIcon
+    <div className={s.Icon}>{leftIcon || rightIcon}</div>
   );
 
   return (
