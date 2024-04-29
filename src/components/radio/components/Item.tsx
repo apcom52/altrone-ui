3178aeb23717fd338430311c20e51391da593f45
Alexander Perevezentsev <apcom@live.ru>
2024-04-29 10:35:42 +0400

    (Link T-433): implemented configuration

---
+++ src/components/radio/components/Item.tsx
@@ -0,0 +1,61 @@
+import { KeyboardEventHandler, memo, useRef } from 'react';
+import { RadioItemProps } from '../Radio.types.ts';
+import s from './item.module.scss';
+import clsx from 'clsx';
+import { useRadioContext } from '../Radio.tsx';
+
+export const RadioItem = memo<RadioItemProps>(
+  ({ children, value, className, disabled, ...restProps }) => {
+    const {
+      value: radioValue,
+      disabled: radioDisabled,
+      name,
+      onChange,
+    } = useRadioContext();
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const itemChecked = radioValue === value;
+    const itemDisabled = Boolean(radioDisabled || disabled);
+
+    const onKeyDown: KeyboardEventHandler = (e) => {
+      if (e.key === 'Enter' || e.key === ' ') {
+        inputRef.current?.click();
+      }
+    };
+
+    const cls = clsx(
+      s.RadioItem,
+      {
+        [s.Checked]: itemChecked,
+        [s.Disabled]: itemDisabled,
+      },
+      className,
+    );
+
+    return (
+      <label
+        role="radio"
+        aria-disabled={itemDisabled}
+        aria-checked={itemChecked}
+        tabIndex={disabled ? -1 : 0}
+        className={cls}
+        onKeyDown={onKeyDown}
+        {...restProps}
+      >
+        <input
+          type="radio"
+          name={name}
+          ref={inputRef}
+          checked={itemChecked}
+          value={value}
+          onChange={onChange}
+          className={s.Input}
+          disabled={disabled}
+        />
+        <div className={s.Button} />
+        <div className={s.Label}>{children}</div>
+      </label>
+    );
+  },
+);
