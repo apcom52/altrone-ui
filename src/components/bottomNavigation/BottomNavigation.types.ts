8590a9baaea825200848a800e3953fe9178cc035
Alexander Perevezentsev <apcom@live.ru>
2024-06-11 12:26:26 +0400

    feat[Link T-613]: added renderFunc for BottomNavigation->Item

--- src/components/bottomNavigation/BottomNavigation.types.ts
+++ src/components/bottomNavigation/BottomNavigation.types.ts
@@ -1,11 +1,13 @@
 import { ReactElement } from 'react';
+import { RenderFuncProp } from '../../types';
 
 export interface BottomNavigationProps
   extends React.HTMLAttributes<HTMLDivElement> {}
 
 export interface BottomNavigationItemProps
-  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'children'> {
+  extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'children'> {
   icon: ReactElement;
   label: string;
   selected?: boolean;
+  renderFunc?: RenderFuncProp<HTMLAnchorElement, BottomNavigationItemProps>;
 }
