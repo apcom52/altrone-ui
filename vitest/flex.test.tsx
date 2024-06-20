590b8cc204fc94569cd0b1beff65920b35e13eaf
Alexander Perevezentsev <apcom@live.ru>
2024-06-20 11:47:01 +0400

    test[Link T-591]: added Flex tests

--- vitest/flex.test.tsx
+++ vitest/flex.test.tsx
@@ -1,13 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import { Configuration, Flex } from '../src';
+
 describe('Flex', () => {
   test('check that properties works correctly', () => {
-    expect(true).toBeFalsy();
+    const { rerender } = render(
+      <Flex data-testid="flex">
+        <div>flex content</div>
+      </Flex>,
+    );
+
+    expect(screen.getByText('flex content')).toBeInTheDocument();
+    expect(screen.getByTestId('flex').tagName).toBe('DIV');
+
+    rerender(
+      <Flex tagName="span" data-testid="flex">
+        <div>flex content</div>
+      </Flex>,
+    );
+
+    expect(screen.getByTestId('flex').tagName).toBe('SPAN');
   });
 
   test('check that custom className and styles works', () => {
-    expect(true).toBeFalsy();
+    render(
+      <Flex className="cls" style={{ fontSize: '20px' }} data-testid="flex">
+        <div>flex content</div>
+      </Flex>,
+    );
+
+    expect(screen.getByTestId('flex')).toHaveClass('cls');
+    expect(screen.getByTestId('flex')).toHaveStyle('font-size: 20px');
   });
 
   test('check that configuration works', () => {
-    expect(true).toBeFalsy();
+    render(
+      <Configuration flex={{ className: 'cls', style: { color: 'blue' } }}>
+        <Flex data-testid="element">content</Flex>
+      </Configuration>,
+    );
+
+    const element = screen.getByTestId('element');
+    expect(element).toHaveClass('cls');
+    expect(element).toHaveStyle('color: blue');
   });
 });
