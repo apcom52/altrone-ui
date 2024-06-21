f197666dd3420b4c9d72a2cef8f15de4a229d078
Alexander Perevezentsev <apcom@live.ru>
2024-06-21 16:38:29 +0400

    test[Link T-594]: added Tooltip tests

--- vitest/tooltip.test.tsx
+++ vitest/tooltip.test.tsx
@@ -1,23 +1,52 @@
 import { render, screen } from '@testing-library/react';
-import { Configuration, List } from '../src/components';
+import { Configuration, List, Tooltip } from '../src/components';
 
 describe('Tooltip', () => {
   test('check that render menu correctly', () => {
-    expect(false).toBeTruthy();
+    const { rerender } = render(
+      <Tooltip content="Tooltip content" openedByDefault />,
+    );
+
+    expect(screen.getByText('Tooltip content')).toBeInTheDocument();
+    expect(screen.getByText('help_outline')).toBeInTheDocument();
+
+    rerender(
+      <Tooltip content="Tooltip content" openedByDefault>
+        source
+      </Tooltip>,
+    );
+
+    expect(screen.getByText('Tooltip content')).toBeInTheDocument();
+    expect(screen.getByText('source')).toBeInTheDocument();
   });
 
   test('check that className and style props works', () => {
-    expect(false).toBeTruthy();
+    render(
+      <Tooltip
+        content="Tooltip content"
+        openedByDefault
+        className="cls"
+        style={{ color: 'blue' }}
+      />,
+    );
+
+    expect(screen.getByText('Tooltip content')).toHaveClass('cls');
+    expect(screen.getByText('Tooltip content')).toHaveStyle('color: blue');
   });
 
-  test('check that Scrollable configuration works correctly', () => {
+  test('check that Tooltip configuration works correctly', () => {
     render(
-      <Configuration list={{ className: 'cls', style: { color: 'blue' } }}>
-        <List data={[]} renderItem={() => <div />} data-testid="element" />
+      <Configuration tooltip={{ className: 'cls', style: { color: 'blue' } }}>
+        <Tooltip
+          content="Tooltip content"
+          openedByDefault
+          className="cls"
+          style={{ color: 'blue' }}
+        />
       </Configuration>,
     );
 
-    const element = screen.getByTestId('element');
+    const element = screen.getByText('Tooltip content');
     expect(element).toHaveClass('cls');
     expect(element).toHaveStyle('color: blue');
   });
