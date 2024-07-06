d381770da1c683c36a86fc81adaaf31776d53ffa
Alexander Perevezentsev <apcom@live.ru>
2024-07-06 13:01:43 +0400

    test: Button unit tests

--- vitest/button.test.tsx
+++ vitest/button.test.tsx
@@ -1,10 +1,14 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { vi } from 'vitest';
-import { Button, Icon } from '../src';
+import { AltroneApplication, Button, Icon } from '../src';
 
 describe('Button', () => {
   test('need to render only icon when no label was passed', () => {
-    render(<Button leftIcon={<Icon i="check" />} data-testid="element" />);
+    render(
+      <AltroneApplication>
+        <Button leftIcon={<Icon i="check" />} data-testid="element" />
+      </AltroneApplication>,
+    );
 
     const element = screen.getByTestId('element');
     expect(element.children).toHaveLength(1);
@@ -12,12 +16,14 @@ describe('Button', () => {
 
   test('need to render left and right icons', () => {
     render(
-      <Button
-        leftIcon={<Icon i="check" />}
-        rightIcon={<Icon i="forward" />}
-        label="Button label"
-        data-testid="element"
-      />,
+      <AltroneApplication>
+        <Button
+          leftIcon={<Icon i="check" />}
+          rightIcon={<Icon i="forward" />}
+          label="Button label"
+          data-testid="element"
+        />
+      </AltroneApplication>,
     );
 
     const buttonLabel = screen.getByText('Button label');
@@ -33,14 +39,44 @@ describe('Button', () => {
     const onClickHandler = vi.fn();
 
     render(
-      <Button
-        leftIcon={<Icon i="check" />}
-        data-testid="element"
-        onClick={onClickHandler}
-      />,
+      <AltroneApplication>
+        <Button
+          leftIcon={<Icon i="check" />}
+          data-testid="element"
+          onClick={onClickHandler}
+        />
+      </AltroneApplication>,
     );
 
     fireEvent.click(screen.getByTestId('element'));
     expect(onClickHandler).toBeCalledTimes(1);
+  });
+
+  test('check that custom className and styles works', () => {
+    render(
+      <Button
+        data-testid="button"
+        className="cls"
+        style={{
+          color: 'blue',
+        }}
+      />,
+    );
+    expect(screen.getByTestId('button')).toHaveClass('cls');
+    expect(screen.getByTestId('button')).toHaveStyle('color: blue');
+  });
+
+  test('check that configuration works', () => {
+    render(
+      <AltroneApplication
+        config={{ button: { className: 'cls', style: { color: 'blue' } } }}
+      >
+        <Button data-testid="button" />
+      </AltroneApplication>,
+    );
+
+    const element = screen.getByTestId('button');
+    expect(element).toHaveClass('cls');
+    expect(element).toHaveStyle('color: blue');
   });
 });
