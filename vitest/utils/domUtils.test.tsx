cf8ca83a22d7af773add4de48427588fe2211b69
Alexander Perevezentsev <apcom@live.ru>
2024-09-29 00:14:50 +0400

    feat[Link T-826]: implemented getValidChildren, hasValidChildren and containsElementType in DOMUtils

--- vitest/utils/domUtils.test.tsx
+++ vitest/utils/domUtils.test.tsx
@@ -1,4 +1,13 @@
-import { DOMUtils } from '../../src';
+import {
+  Button,
+  DOMUtils,
+  Dropdown,
+  Flex,
+  Icon,
+  NavigationList,
+  Popover,
+  TextInput,
+} from '../../src';
 import { Fragment } from 'react';
 
 describe('DomUtils', () => {
@@ -6,5 +15,90 @@ describe('DomUtils', () => {
     expect(DOMUtils.isFragment(<Fragment />)).toBe(true);
     expect(DOMUtils.isFragment(<div />)).toBe(false);
     expect(DOMUtils.isFragment(null)).toBe(false);
+  });
+
+  test('hasValidChildren', async () => {
+    expect(DOMUtils.hasValidChildren(<div />)).toBe(false);
+    expect(DOMUtils.hasValidChildren(<div>test</div>)).toBe(false);
+    expect(DOMUtils.hasValidChildren(<div>123</div>)).toBe(false);
+    expect(DOMUtils.hasValidChildren(<div>{null}</div>)).toBe(false);
+    expect(
+      DOMUtils.hasValidChildren(
+        <div>
+          <span />
+        </div>,
+      ),
+    ).toBe(true);
+    expect(
+      DOMUtils.hasValidChildren(
+        <div>
+          <span>
+            <span />
+          </span>
+        </div>,
+      ),
+    ).toBe(true);
+  });
+
+  test('containsElementType', async () => {
+    expect(DOMUtils.containsElementType(<Button label="abc" />, Button)).toBe(
+      true,
+    );
+    expect(
+      DOMUtils.containsElementType(<Button label="abc" />, TextInput),
+    ).toBe(false);
+
+    expect(
+      DOMUtils.containsElementType(
+        <Popover content={null}>
+          <Button label="abc" />
+        </Popover>,
+        Popover,
+      ),
+    ).toBe(true);
+
+    expect(
+      DOMUtils.containsElementType(
+        <Popover content={null}>
+          <Button label="abc" />
+          <TextInput />
+          <Flex>
+            <Icon i="abc" />
+          </Flex>
+        </Popover>,
+        Button,
+      ),
+    ).toBe(true);
+
+    expect(
+      DOMUtils.containsElementType(
+        <Popover content={null}>
+          <Button label="abc" />
+          <TextInput />
+          <Flex>
+            <Icon i="abc" />
+          </Flex>
+        </Popover>,
+        Icon,
+      ),
+    ).toBe(true);
+
+    expect(
+      DOMUtils.containsElementType(
+        <Popover content={null}>
+          <NavigationList.GroupAction label="abc" icon={<Icon i="test" />} />
+        </Popover>,
+        NavigationList.GroupAction,
+      ),
+    ).toBe(true);
+
+    expect(
+      DOMUtils.containsElementType(
+        <Dropdown content={null}>
+          <NavigationList.GroupAction label="abc" icon={<Icon i="test" />} />
+        </Dropdown>,
+        NavigationList.GroupAction,
+      ),
+    ).toBe(true);
   });
 });
