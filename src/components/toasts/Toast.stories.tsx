0f6c76e2005951fd912df63eb6ca543aa0534133
Alexander Perevezentsev <apcom@live.ru>
2024-10-16 12:16:36 +0400

    feat[T-10]: added sendNotification function for useToast hook

--- src/components/toasts/Toast.stories.tsx
+++ src/components/toasts/Toast.stories.tsx
@@ -1,7 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Button, Flex, Text } from 'components';
+import { Button, Checkbox, Dropdown, Flex, Select, Text } from 'components';
 import { StorybookDecorator } from 'global/storybook';
 import { Toast, useToast } from './Toast.tsx';
+import { useBoolean } from '../../utils';
+import { useState } from 'react';
 
 const story: Meta<typeof Toast> = {
   title: 'Components/Display/Toast',
@@ -12,7 +14,7 @@ const story: Meta<typeof Toast> = {
   parameters: {},
 };
 
-export const FlexLayout: StoryObj<typeof Flex> = {
+export const SimpleToastsStory: StoryObj<typeof Flex> = {
   name: 'Using Toasts',
   render: (args) => {
     const { toast, success, warning, danger } = useToast();
@@ -38,6 +40,92 @@ export const FlexLayout: StoryObj<typeof Flex> = {
           <Button
             label="Danger toast"
             onClick={() => danger('An error occurred. Please try again')}
+          />
+        </Flex>
+      </Flex>
+    );
+  },
+};
+
+const NOTIFICATION_PLACEMENT = [
+  { label: 'Top Left', value: 'top-left' },
+  { label: 'Top Right', value: 'top-right' },
+  { label: 'Bottom Left', value: 'bottom-left' },
+  { label: 'Bottom Right', value: 'bottom-right' },
+];
+
+export const NotificationsStory: StoryObj<typeof Flex> = {
+  name: 'Using Notifications',
+  render: () => {
+    const { value: titleVisible, setValue: setTitleVisible } = useBoolean(true);
+    const { value: iconVisible, setValue: setIconVisible } = useBoolean(true);
+    const { value: actionVisible, setValue: setActionVisible } =
+      useBoolean(true);
+
+    const [placement, setPlacement] = useState<
+      'top-left' | 'top-right' | 'bottom-left' | 'bottom-right' | undefined
+    >('top-right');
+
+    const { sendNotification } = useToast();
+
+    return (
+      <Flex gap="l" direction="vertical">
+        <Text.Heading variant="inner">
+          What need to show in notification?
+        </Text.Heading>
+        <Flex gap="l">
+          <Checkbox checked={titleVisible} onChange={setTitleVisible}>
+            Title
+          </Checkbox>
+          <Checkbox checked={iconVisible} onChange={setIconVisible}>
+            Icon
+          </Checkbox>
+          <Checkbox checked={actionVisible} onChange={setActionVisible}>
+            Action
+          </Checkbox>
+        </Flex>
+        <Text.Heading variant="inner">
+          Where we need to show this notification?
+        </Text.Heading>
+        <Flex gap="l">
+          <Select
+            value={placement}
+            onChange={setPlacement}
+            options={NOTIFICATION_PLACEMENT}
+          />
+        </Flex>
+        <Text.Heading variant="inner">Click to show</Text.Heading>
+        <Flex direction="horizontal" gap="l">
+          <Button
+            label="Show notification"
+            onClick={() =>
+              sendNotification({
+                message: 'New updates are available.',
+                title: titleVisible ? 'New message from Karen Wok' : undefined,
+                icon: iconVisible ? (
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/281/281769.png"
+                    width={48}
+                    height={48}
+                  />
+                ) : undefined,
+                action: actionVisible ? (
+                  <Dropdown
+                    content={
+                      <Dropdown.Menu>
+                        <Dropdown.Action label="Open" />
+                        <Dropdown.Action label="Remind later" />
+                        <Dropdown.Action label="Close" />
+                      </Dropdown.Menu>
+                    }
+                  >
+                    <Button label="Update" />
+                  </Dropdown>
+                ) : undefined,
+                placement: placement ?? 'top-right',
+                duration: 3000,
+              })
+            }
           />
         </Flex>
       </Flex>
