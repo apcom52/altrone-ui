5ae5b8cef34e2f62f318e5c5ba2356333967b07e
Alexander Perevezentsev <apcom@live.ru>
2024-12-03 20:19:11 +0400

    tests: added some test cases for DataTable

--- vitest/dataTable.test.tsx
+++ vitest/dataTable.test.tsx
@@ -5,7 +5,10 @@ import {
   DataTable,
 } from '../src/components';
 import { EMPLOYEES } from '../src/components/dataTable/stories/EMPLOYEES';
-import { INVOICES } from '../src/components/dataTable/stories/INVOICES';
+import {
+  INVOICES,
+  INVOICES2,
+} from '../src/components/dataTable/stories/INVOICES';
 
 describe('DataTable', () => {
   test('test DataTable cell renderers', () => {
@@ -24,6 +27,19 @@ describe('DataTable', () => {
                 currencyAccessor: 'currency',
               },
             },
+            {
+              accessor: 'price',
+              type: 'currency',
+              label: 'Price',
+              options: {
+                currency: 'EUR',
+              },
+            },
+            {
+              accessor: 'price',
+              type: 'currency',
+              label: 'Price',
+            },
             { accessor: 'date', type: 'date', label: 'Date' },
             { accessor: 'date', type: 'month', label: 'Month' },
             { accessor: 'date', type: 'year', label: 'Year' },
@@ -38,10 +54,50 @@ describe('DataTable', () => {
 
     expect(table.rows[1].cells[0].textContent).toBe('Web Development');
     expect(table.rows[1].cells[1].textContent).toBe('1');
-    expect(table.rows[1].cells[2].textContent).toBe('$1,500.00');
-    expect(table.rows[1].cells[3].textContent).toBe('January 15, 2024');
-    expect(table.rows[1].cells[4].textContent).toBe('January 2024');
-    expect(table.rows[1].cells[5].textContent).toBe('2024');
+    expect(table.rows[3].cells[2].textContent).toBe('£1,200.00');
+    expect(table.rows[3].cells[3].textContent).toBe('€1,200.00');
+    expect(table.rows[3].cells[4].textContent).toBe('$1,200.00');
+    expect(table.rows[1].cells[5].textContent).toBe('January 15, 2024');
+    expect(table.rows[1].cells[6].textContent).toBe('January 2024');
+    expect(table.rows[1].cells[7].textContent).toBe('2024');
+  });
+
+  test('test DataTable cell renderers [2]', () => {
+    render(
+      <AltroneApplication config={{ locale: { locale: 'en-US' } }}>
+        <DataTable
+          data={INVOICES2}
+          columns={[
+            {
+              accessor: 'tags',
+              type: 'array',
+              label: 'Users',
+              options: { arrayDelimiter: '; ' },
+            },
+            {
+              accessor: 'users',
+              type: 'array',
+              label: 'Users',
+              options: { arrayAccessor: 'name' },
+            },
+            { accessor: 'isPaid', type: 'boolean', label: 'Paid' },
+          ]}
+          data-testid="data-table"
+          id="table"
+        />
+      </AltroneApplication>,
+    );
+
+    const table = document.querySelector('#table') as HTMLTableElement;
+
+    expect(table.rows[1].cells[0].textContent).toBe('finance; monthly');
+    expect(table.rows[1].cells[1].textContent).toBe('John Doe, Jane Smith');
+    expect(table.rows[1].cells[2].querySelector('span')).toHaveTextContent(
+      'check',
+    );
+    expect(table.rows[2].cells[2].querySelector('span')).toHaveTextContent(
+      'close',
+    );
   });
 
   test('check that className and style props works', () => {
