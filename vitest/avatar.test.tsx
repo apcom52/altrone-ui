4481daa27fd789ce565cc69f2676a703f1e8577d
Alexander Perevezentsev <apcom@live.ru>
2025-04-24 23:44:39 +0300

    test [T-102]: add unit tests for Avatar and DummyBox components

---
+++ vitest/avatar.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { expect, test, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { Avatar, Configuration } from '../src';
+
+describe('Avatar', () => {
+  test('check avatar works correctly', () => {
+    const { rerender } = render(<Avatar firstName="John" lastName="Doe" />);
+    expect(screen.getByText('JD')).toBeInTheDocument();
+
+    rerender(<Avatar firstName="John" />);
+    expect(screen.getByText('J')).toBeInTheDocument();
+
+    rerender(<Avatar firstName="Mark" lastName="Zoe" />);
+    expect(screen.getByText('MZ')).toBeInTheDocument();
+  });
+
+  test('check that properties works correctly', () => {
+    render(
+      <Avatar
+        data-testid="element"
+        firstName="John"
+        lastName="Doe"
+        className="cls"
+        style={{ color: 'rgb(0, 0, 255)' }}
+      />,
+    );
+
+    expect(screen.getByTestId('element')).toHaveClass('cls');
+    expect(screen.getByTestId('element')).toHaveStyle('color: rgb(0, 0, 255)');
+  });
+
+  test('check that configuration works', () => {
+    render(
+      <Configuration
+        avatar={{ className: 'cls', style: { color: 'rgb(0, 0, 255)' } }}
+      >
+        <Avatar data-testid="element" firstName="John" lastName="Doe" />
+      </Configuration>,
+    );
+
+    const element = screen.getByTestId('element');
+    expect(element).toHaveClass('cls');
+    expect(element).toHaveStyle('color: rgb(0, 0, 255)');
+  });
+});
